a49a2c469fed02748d8547bd830b7d6c66d10660
ordinary-developer <merely.ordinary.developer@gmail.com>
2018-02-08 22:32:00 +0300

    [visual_cpp_and_mfc_programming-2_ed][ch11] add the 5-th example
    add the 5-th example from chapter 11

---
+++ books/techno/cpp/mfc/visual_cpp_and_mfc_programming-2_ed/ch_11-introduction_to_windows_controls/05-completing_the_control/Project/Project.h
@@ -0,0 +1,32 @@
+
+// Project.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CProjectApp:
+// See Project.cpp for the implementation of this class
+//
+
+class CProjectApp : public CWinApp
+{
+public:
+	CProjectApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CProjectApp theApp;
