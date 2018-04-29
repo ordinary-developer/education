890fffbf0c0896df94f565fe9cf6736e8651c506
ordinary-developer <merely.ordinary.developer@gmail.com>
2018-04-29 22:04:25 +0300

    [programming_windows_with_mfc-2_ed-j_prosice][ch_06] add the 7-th example
    add the 7-th example from chapter 6
    (using a versioning for serializable classes)

---
+++ books/techno/cpp/mfc/programming_windows_with_mfc-2_ed-j_prosise/ch_06-file_io_and_serialization/07-versioning_serializable_classes/project/project.cpp
@@ -0,0 +1,94 @@
+#include "project.h"
+
+#include "afxtempl.h"
+
+#include <string>
+#include <cstddef>
+
+CMyApp myApp;
+
+BOOL CMyApp::InitInstance()
+{
+	m_pMainWnd = new CMainWindow;
+
+	m_pMainWnd->ShowWindow(m_nCmdShow);
+	m_pMainWnd->UpdateWindow();
+	return TRUE;
+}
+
+BEGIN_MESSAGE_MAP (CMainWindow, CFrameWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+CMainWindow::CMainWindow() {
+	Create(nullptr, _T("app example"));
+}
+
+void CMainWindow::OnPaint() {
+
+}
+
+void CMainWindow::OnLButtonDown(UINT nFlags, CPoint point) {
+	example_01();
+}
+
+void CMainWindow::example_01() {
+	const std::size_t COUNT = 3;
+	CLine* pLines[COUNT] = { new CLine(5, 10), new CLine(15, 20), new CLine(25, 30) };
+
+	{
+		CFile storeFile(_T("example_01"), CFile::modeWrite | CFile::modeCreate);
+		CArchive storeAr(&storeFile, CArchive::store);
+
+		storeAr << COUNT;
+		for (std::size_t i = 0; i < COUNT; ++i)
+			storeAr << pLines[i];
+	}
+
+	{
+		CFile loadFile(_T("example_01"), CFile::modeRead);
+		CArchive loadAr(&loadFile, CArchive::load);
+
+		std::size_t destCount = 0;
+		loadAr >> destCount;
+
+		CLine** pDestLines = new CLine*[destCount];
+		for (std::size_t i = 0; i < destCount; ++i)
+			loadAr >> pDestLines[i];
+
+		for (std::size_t i = 0; i < destCount; ++i) {
+			delete pDestLines[i];
+			pDestLines[i] = nullptr;
+		}
+	}
+
+	for (std::size_t i = 0; i < COUNT; ++i) {
+		delete pLines[i];
+		pLines[i] = nullptr;
+	}
+
+}
+
+IMPLEMENT_SERIAL(CLine, CObject, 2 | VERSIONABLE_SCHEMA)
+
+void CLine::Serialize(CArchive& ar) {
+	CObject::Serialize(ar);
+	if (ar.IsStoring())
+		ar << m_ptFrom << m_ptTo << m_clrLine;
+	else {
+		UINT nSchema = ar.GetObjectSchema();
+		switch (nSchema) {
+			case 1:
+				ar >> m_ptFrom >> m_ptTo;
+				m_clrLine = RGB(0, 0, 0);
+				break;
+			case 2:
+				ar >> m_ptFrom >> m_ptTo >> m_clrLine;
+				break;
+			default:
+				AfxThrowArchiveException(CArchiveException::badSchema);
+				break;
+		}
+	}
+}
