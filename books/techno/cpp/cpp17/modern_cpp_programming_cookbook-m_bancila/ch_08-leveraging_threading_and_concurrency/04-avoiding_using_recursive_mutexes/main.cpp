063c2123b9895f69c66b457108f0844111e6cd01
ordinary-developer <merely.ordinary.developer@gmail.com>
2019-08-19 17:48:51 +0400

    [modern_cpp_programming_cookbook-m_bancila] change example 08.04

--- books/techno/cpp/cpp17/modern_cpp_programming_cookbook-m_bancila/ch_08-leveraging_threading_and_concurrency/04-avoiding_using_recursive_mutexes/main.cpp
+++ books/techno/cpp/cpp17/modern_cpp_programming_cookbook-m_bancila/ch_08-leveraging_threading_and_concurrency/04-avoiding_using_recursive_mutexes/main.cpp
@@ -1,19 +1,19 @@
-#include <mutex>
 #include <thread>
-namespace example_01 {
+#include <mutex>
+namespace workspace {
     
-class foo {
+class Foo {
     public:
-        foo(int const d = 0) : data_{ d } { }
+        Foo(int const d = 0) : data_{d} {}
     
         void update(int const d) {
-            std::lock_guard<std::mutex> lock{ m_ };
+            std::lock_guard<std::mutex> lock{ mut_ };
             internal_update(d);
         }
     
-        int update_with_return(int const d) {
-            std::lock_guard<std::mutex> lock{ m_ };
-            int tmp{ data_ };
+        int update_with_ret(int const d) {
+            std::lock_guard<std::mutex> lock{ mut_ };
+            int const tmp{ data_ };
             internal_update(d);
             return tmp;
         }
@@ -21,20 +21,21 @@ class foo {
     private:
         void internal_update(int const d) { data_ = d; }
     
-        std::mutex m_;
+        std::mutex mut_;
         int data_;
 };
     
 void run() {
-    foo f{};
-    std::thread t1{ &foo::update, &f, 5 }, t2{ &foo::update, &f, 10 };
+    Foo foo{};
+    std::thread t1{ &Foo::update, &foo, 5 }, t2{ &Foo::update, &foo, 10 };
     t1.join(); t2.join();
 }    
+    
+} // workspace
 
-} // example_01
 
 int main() {
-    example_01::run();
+    workspace::run();
     
     return 0;
 }
