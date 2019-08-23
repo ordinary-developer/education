7f9fb208510e9e178c90d5783bf84d8a22bb71af
ordinary-developer <merely.ordinary.developer@gmail.com>
2019-08-23 16:50:22 +0400

    [cpp_std_lib-2_ed-n_m_josuttis][18] add using non default memory orders

---
+++ books/techno/cpp/cpp17/cpp_std_lib-2_ed-n_m_josuttis/18-concurrency/11-atomic_bool_with_other_memory_orders/main.cpp
@@ -0,0 +1,41 @@
+#include <atomic>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+namespace workspace {
+    
+long data{};
+std::atomic<bool> readyFlag{ false };
+    
+void provider() {
+    std::cout << "<return>" << std::endl;
+    std::cin.get();
+    
+    data = 42;
+    
+    readyFlag.store(true, std::memory_order_release);
+}
+    
+void consumer() {
+    while (not readyFlag.load(std::memory_order_acquire)) {
+        std::cout.put('.').flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds{ 500 });
+    }
+    
+    std::cout << "\nvalue : " << data << std::endl;
+}    
+    
+void run() {
+    auto p = std::async(std::launch::async, provider);
+    auto c = std::async(std::launch::async, consumer);
+}    
+} // workspace
+
+
+int main() {
+    workspace::run();
+    
+    return 0;
+}
