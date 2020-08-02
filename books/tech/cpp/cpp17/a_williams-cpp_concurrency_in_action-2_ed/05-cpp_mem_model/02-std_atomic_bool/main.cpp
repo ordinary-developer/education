dca78eb7338d72e6e857d7278b18924633cb5769
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-02 18:29:00 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][ch_05] change example N 2
    change (slightly rewrite) the example for std::atomic<bool>

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/02-std_atomic_bool/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/02-std_atomic_bool/main.cpp
@@ -1,23 +1,22 @@
 #include <functional>
 #include <random>
-namespace { // utilities
+namespace { // util func
 bool rand_bool() {
-    auto gen = std::bind(
+    return std::bind(
         std::uniform_int_distribution<>{ 0, 1 },
-        std::default_random_engine{});
-    return gen();
-}
+        std::default_random_engine{})();
+}    
+} // anonymous namespace
 
-} // anonymous
 
 #include <atomic>
 #include <iostream>
 #include <cassert>
-namespace example_01 { // assignment operator for std::atomic<bool>
+namespace example_01 { // asignment operator for std::atomic<bool>
 void run() {
     std::atomic<bool> atom_val{true};
     bool const val = (atom_val = false);
-    assert(false == val);
+    assert(not val and not atom_val);
 }    
 } // example_01
 
@@ -28,68 +27,68 @@ void run() {
 namespace example_02 { // "load" and "store" operations for std::atomic<bool>
 void run() {
     std::atomic<bool> atom_val{};
-
+    
     {
-        bool const val = atom_val.load(std::memory_order_acquire);
-        assert(not val);
+    assert(not atom_val.load());
     }
-
+    
     {
-        atom_val.store(true);
-        bool const val_before = atom_val.exchange(false, std::memory_order_acq_rel);
-        bool const val_after = atom_val;
-        assert(val_before);
-        assert(not val_after);
+    atom_val.store(true);
+        
+    bool const val_before = atom_val.exchange(false);
+    bool const val_after = atom_val;
+            
+    assert(val_before);
+    assert(not val_after);
     }
-}
+}    
 } // example_02
 
 
-#include <atomic>
-#include <cassert>
 namespace example_03 { // "compare_exchange_weak" using
 void run() {
     bool expected = false;
     std::atomic<bool> atom_val{ rand_bool() };
-    while(not atom_val.compare_exchange_weak(expected, true) and not expected);
+    while (not atom_val.compare_exchange_weak(expected, true) and not expected);
     assert(atom_val);
-}
+}    
 } // example_03
 
 
 #include <atomic>
 #include <cassert>
-namespace example_04 { // "compare_exchange_weak" with memor ordering
+namespace example_04 {
 void run() {
     std::atomic<bool> atom_val{};
     bool expected{};
-
+    
     atom_val.store(rand_bool());
     expected = false;
     while (
         not atom_val.compare_exchange_weak(expected, true,
             std::memory_order_acq_rel, std::memory_order_acquire)
-        and not expected) {}
+        and not expected
+    ) {}
     assert(atom_val);
-
+    
     atom_val.store(rand_bool());
     expected = false;
     while (
         not atom_val.compare_exchange_weak(
             expected, true, std::memory_order_acq_rel)
-        and not expected) {}            
+        and not expected
+    ) {}
     assert(atom_val);
-}
-
+}    
 } // example_04
 
 
-#include <iostream> 
+#include <iostream>
 int main() {
-    std::cout << "example_01 => [OK]"; example_01::run(); std::cout << "\n\n";
-    std::cout << "example_02 => [OK]"; example_02::run(); std::cout << "\n\n";
-    std::cout << "example_03 => [OK]"; example_03::run(); std::cout << "\n\n";
-    std::cout << "example_04 => [OK]"; example_04::run(); std::cout << "\n\n";
-
+    std::cout << "[ok] - example_01" << std::endl; example_01::run(); std::cout << std::endl;
+    std::cout << "[ok] - example_02" << std::endl; example_02::run(); std::cout << std::endl;
+    std::cout << "[ok] - example_03" << std::endl; example_03::run(); std::cout << std::endl;
+    std::cout << "[ok] - example_04" << std::endl; example_04::run(); std::cout << std::endl;
+    
     return 0;
 }
