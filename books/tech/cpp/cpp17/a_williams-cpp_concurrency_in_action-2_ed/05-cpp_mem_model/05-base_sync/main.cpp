e4a6118e81bc5364fd90d1e4d574c4590262219e
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-03 15:01:43 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][5] change 5-th example
    change the example illustrated base synchronization with atomic vars

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/05-base_sync/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/05-base_sync/main.cpp
@@ -4,34 +4,35 @@
 #include <thread>
 #include <vector>
 #include <cassert>
-namespace example_01 { // using atomic types for synchronization
-
+namespace example_01 { // base usage of std::atomic<T*>
+    
 std::vector<int> data{};
-std::atomic<bool> data_ready{false};
-
+std::atomic<bool> data_ready{false};    
+    
 void reader_thread() {
     while (not data_ready.load())
         std::this_thread::sleep_for(std::chrono::milliseconds{1});
+    
     assert(1 == data.size());
-    assert(42 == data[0]);
+    assert(42 == *data.begin());
 }
-
+    
 void writer_thread() {
     data.push_back(42);
     data_ready = true;
 }
-
+    
 void run() {
     std::thread t1{ reader_thread };
     std::thread t2{ writer_thread };
-    t1.join(); t2.join();
+    t1.join(), t2.join();
 }
 } // example_01
 
 
 #include <iostream>
 int main() {
-    std::cout << "example_01 => [OK]"; example_01::run(); std::cout << std::endl << std::endl;
-
+    std::cout << "[ok] - example 01" << std::endl; example_01::run(); std::cout << std::endl;
+    
     return 0;
 }
