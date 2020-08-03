b9bd8f3122b78476aa0493b550cda0c2454000cc
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-03 14:08:01 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][ch_05] update 3-d example
    update code (minor improvements) for the std::atomic<T*> primitive

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/03-std_atomic_pointer/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/03-std_atomic_pointer/main.cpp
@@ -2,29 +2,30 @@
 #include <cassert>
 namespace example_01 { // base usage of std::atomic<T*>
 void run() {
-    class Foo {};    
-
-    Foo some_array[5];
-    std::atomic<Foo*> atomic_pointer{some_array};
-    Foo *pointer_val{nullptr};
-
-    pointer_val = atomic_pointer.fetch_add(2);
-    assert(some_array == pointer_val);
-    assert(&some_array[2] == atomic_pointer.load());
-
-    pointer_val = (atomic_pointer -= 1);
-    assert(&some_array[1] == pointer_val);
-    assert(&some_array[1] == atomic_pointer.load());
-
-    pointer_val = atomic_pointer.fetch_add(3, std::memory_order_release);
-    assert(&some_array[1] == pointer_val);
-    assert(&some_array[4] == atomic_pointer.load());
+    struct foo{};
+    
+    foo some_array[5];
+    std::atomic<foo*> atom_ptr{ some_array };
+    foo* ptr_val{nullptr};
+    
+    ptr_val = atom_ptr.fetch_add(2);
+    assert(some_array == ptr_val);
+    assert(&some_array[2] == atom_ptr.load());
+    
+    ptr_val = (atom_ptr -= 1);
+    assert(&some_array[1] == ptr_val);
+    assert(&some_array[1] == atom_ptr.load());
+    
+    ptr_val = atom_ptr.fetch_add(3, std::memory_order_release);
+    assert(&some_array[1] == ptr_val);
+    assert(&some_array[4] == atom_ptr.load());
 }
 } // example_01
 
+
 #include <iostream>
 int main() {
-    std::cout << "example 01 => [OK]"; example_01::run(); std::cout << "\n\n";
-
+    std::cout << "[ok] - example 01" << std::endl; example_01::run(); std::cout << std::endl;
+    
     return 0;
 }
