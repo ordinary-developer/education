910a1b033eb1c533af25361bb0d7e97804429dcf
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-03 14:48:40 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][5] change the 4-th example
    slightly rewrite the example for atomic funcs for std::shared_ptr<T>

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/04-std_shared_ptr/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/04-std_shared_ptr/main.cpp
@@ -1,38 +1,41 @@
 #include <atomic>
 #include <memory>
 #include <cassert>
-namespace example_01 { // using atomic operations for std::shared_ptr
-
-struct Foo { int val_ = 0; };
-
-std::shared_ptr<Foo> pointer{ new Foo{} };
-
+namespace example_01 { // base usage of std::atomic<T*>
+    
+struct foo { int val = 0; };
+    
+std::shared_ptr<foo> ptr{ new foo{} };
+    
 void process_global_data() {
-    std::shared_ptr<Foo> local{ std::atomic_load(&pointer) };
-    local->val_ = 42;
-
-    assert(42 == std::atomic_load(&pointer)->val_);
+    {
+    std::shared_ptr<foo> local{ std::atomic_load(&ptr) };
+    local->val = 42;
+    }
+    
+    assert(42 == std::atomic_load(&ptr)->val);
 }
-
+    
 void update_global_data() {
-    std::shared_ptr<Foo> local{ new Foo{} };
-    local->val_ = 42;
-    assert(42 == std::atomic_load(&pointer)->val_);
-    std::atomic_store(&pointer, local);
-
-    assert(42 == std::atomic_load(&pointer)->val_);
+    {
+    std::shared_ptr<foo> local{ new foo{} };
+    local->val = 17;
+    std::atomic_store(&ptr, local);
+    }
+    
+    assert(17 == std::atomic_load(&ptr)->val);
 }
-
+    
 void run() {
     process_global_data();
-    update_global_data();
+    update_global_data();    
 }
 } // example_01
 
 
 #include <iostream>
 int main() {
-    std::cout << "example_01 => [OK]"; example_01::run(); std::cout << std::endl << std::endl;
-
+    std::cout << "[ok] - example 01" << std::endl; example_01::run(); std::cout << std::endl;
+    
     return 0;
 }
