75c5dc4e7e98f930cd5a529934e2141d14db46bb
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-04 15:17:04 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][5] change the 7-th example
    change the example illustrated sequential memory ordering usage

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/07-seq_cst_ordering/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/07-seq_cst_ordering/main.cpp
@@ -2,41 +2,39 @@
 #include <thread>
 #include <cassert>
 namespace example_01 { // example of std::memory_order_seq_cst usage
-
+    
 std::atomic<bool> x{}, y{};
 std::atomic<int> z{};
-
+    
 void write_x() { x.store(true, std::memory_order_seq_cst); }
 void write_y() { y.store(true, std::memory_order_seq_cst); }
-
+    
 void read_x_then_y() {
     while (not x.load(std::memory_order_seq_cst));
     if (y.load(std::memory_order_seq_cst))
         ++z;
 }
-
+    
 void read_y_then_x() {
     while (not y.load(std::memory_order_seq_cst));
     if (x.load(std::memory_order_seq_cst))
         ++z;
 }
-
+    
 void run() {
     x = false, y = false, z = 0;
-
+    
     std::thread t1{ write_x }, t2{ write_y }, t3{ read_x_then_y }, t4{ read_y_then_x };
-    t1.join(); t2.join(); t3.join(); t4.join();
-
+    t1.join(), t2.join(), t3.join(), t4.join();
+    
     assert(0 != z.load());
 }
-
-}
+} // example_01
 
 
 #include <iostream>
-
 int main() {
-    std::cout << "example_01 => [OK]"; example_01::run(); std::cout << std::endl << std::endl;
-
+    std::cout << "[ok] - example_01" << std::endl; example_01::run(); std::cout << std::endl;
+    
     return 0;
 }
