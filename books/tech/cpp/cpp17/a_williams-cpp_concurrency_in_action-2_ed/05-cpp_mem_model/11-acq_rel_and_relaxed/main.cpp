f40fdf3a61b1767485624618460638780b976901
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-05 14:00:02 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][5] upd the 11-th example
    add the example for acquire_release and relaxed memory models

--- books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/11-acq_rel_and_relaxed/main.cpp
+++ books/tech/cpp/cpp17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/11-acq_rel_and_relaxed/main.cpp
@@ -1,36 +1,34 @@
 #include <atomic>
 #include <thread>
 #include <cassert>
-namespace example_01 {  // acquire-release and relaxed orederings
-
+namespace example_01 { // acquire-release and relaxed orderings
+    
 std::atomic<bool> x{}, y{};
 std::atomic<int> z{};
-
+    
 void write_x_then_y() {
     x.store(true, std::memory_order_relaxed);
     y.store(true, std::memory_order_release);
 }
-
+    
 void read_y_then_x() {
     while (not y.load(std::memory_order_acquire));
     if (x.load(std::memory_order_relaxed))
         ++z;
 }
-
+    
 void run() {
     x = false, y = false, z = 0;
     std::thread t1{ write_x_then_y }, t2{ read_y_then_x };
-    t1.join(); t2.join();
-
-    assert(0 != z.load());
-}
-
+    t1.join(), t2.join();
+    
+    assert(1 == z.load());
+}    
 } // example_01
-
 
 #include <iostream>
 int main() {
-    std::cout << "example_01 => [OK]"; example_01::run(); std::cout << std::endl << std::endl;
-
+    std::cout << "[ok] - example_01" << std::endl; example_01::run(); std::cout << std::endl;
+    
     return 0;
 }
