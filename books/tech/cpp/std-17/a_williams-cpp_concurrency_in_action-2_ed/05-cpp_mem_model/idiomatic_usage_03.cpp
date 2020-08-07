1b1ffcc26dc2b726912b8d027676c0841fe24d0b
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-07 15:42:39 +0400

    [a_williams-cpp_concurrency_in_action-2_ed][05] add the 3-d code example
    add the 3-d idiomatic code example for atomics

---
+++ books/tech/cpp/std-17/a_williams-cpp_concurrency_in_action-2_ed/05-cpp_mem_model/idiomatic_usage_03.cpp
@@ -0,0 +1,34 @@
+#include <atomic>
+#include <thread>
+#include <cassert>
+namespace workspace {
+
+std::atomic<bool> x{}, y{};
+std::atomic<int> z{};
+    
+void write_x_then_y() {
+    x.store(true, std::memory_order_relaxed);
+    y.store(true, std::memory_order_release);
+}
+    
+void read_y_then_x() {
+    while (not y.load(std::memory_order_acquire));
+    if (x.load(std::memory_order_relaxed))
+        ++z;
+}
+    
+void run() {
+    x = false, y = false, z = 0;
+    std::thread t1{ write_x_then_y }, t2{ read_y_then_x };
+    t1.join(), t2.join();
+    
+    assert(1 == z.load());
+}
+} // workspace
+
+
+int main() {
+    workspace::run();
+    
+    return 0;
+}
