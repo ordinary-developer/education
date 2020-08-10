cb136d88ea398f50255ac05383e1879e5d9085b7
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-08-10 22:08:45 +0400

    [i_cukic-func_progr_in_cpp][02] add the 5-th example
    add another example for std::copy and std::transform

---
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_02-getting_started_with_fp/05-copy_and_transform-2/main.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+
+namespace {
+class person_t final {
+    public:
+        enum gender_t { male, female, other };
+
+        person_t() : name_{ "none" }, gender_{ gender_t::male } { }
+
+        person_t(std::string const& name, gender_t const gender)
+            : name_{ name }, gender_{ gender } {}
+
+        std::string name() const { return name_; }
+        gender_t gender() const { return gender_; }
+
+        friend std::ostream& operator << (std::ostream&, person_t const&);
+
+    private:
+        std::string name_;
+        gender_t gender_;
+};
+
+std::ostream& operator << (std::ostream& out, person_t const& person) {
+    out << person.name() << " ";
+    return out;
+}
+} // anonymous namespace
+
+
+std::string name(person_t const& person) { return person.name(); }
+bool is_female(person_t const& person) { return person_t::female == person.gender(); }
+bool is_not_female(person_t const& person) { return not is_female(person); }
+
+int main() {
+    std::vector<person_t> people {
+        { "David",  person_t::male   },
+        { "Jane",   person_t::female },
+        { "Martha", person_t::female },
+        { "Peter",  person_t::male   },
+        { "Rose",   person_t::female },
+        { "Tom",    person_t::male   }
+    };
+
+    std::vector<person_t> separated(people.size());
+    const auto last = std::copy_if(
+        std::cbegin(people), std::cend(people),
+        separated.begin(),
+        is_female);
+    std::copy_if(std::cbegin(people), std::cend(people),
+        last, is_not_female);                 
+
+    std::copy(std::cbegin(separated), std::cend(separated),
+        std::ostream_iterator<person_t>(std::cout, " "));              
+
+    return 0;
+}
