01ebdc923140fbec9a7de845e19b585727812440
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-09-13 18:37:05 +0300

    [m_shlee-qt_5_10][36] slightly change the 2-nd example
    Slightly change the example devoted to QBuffer

--- books/tech/cpp/qt/m_shlee-qt_5_10/36-filesystem/02-q_buffer/main.cpp
+++ books/tech/cpp/qt/m_shlee-qt_5_10/36-filesystem/02-q_buffer/main.cpp
@@ -3,16 +3,14 @@
 #include <QDataStream>
 #include <QFile>
 #include <QDebug>
-
-
 void run() {
     QByteArray data{};
-
+    
     QBuffer buf{&data};
     buf.open(QIODevice::WriteOnly);
     QDataStream out{&buf};
     out << QString{"msg"};
-
+    
     QFile file{ "file.dat" };
     if (not file.open(QIODevice::WriteOnly)) {
         qDebug() << "error with file opening for writing";
@@ -20,7 +18,7 @@ void run() {
     }
     file.write(data);
     file.close();
-
+    
     QFile::remove(file.fileName());
 }
 
