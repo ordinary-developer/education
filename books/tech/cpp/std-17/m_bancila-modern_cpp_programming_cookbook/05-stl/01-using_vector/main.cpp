e5ce9c8558f2941c6e82e48e9b63cdb14c2400f1
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-11-09 21:07:59 +0300

    [m_bancila-modern_cpp_programming_cookbook][ch_05] change the 1-st example
    change an example devoted to using std::vector as a default container
    (minor changes)

--- books/tech/cpp/std-17/m_bancila-modern_cpp_programming_cookbook/05-stl/01-using_vector/main.cpp
+++ books/tech/cpp/std-17/m_bancila-modern_cpp_programming_cookbook/05-stl/01-using_vector/main.cpp
@@ -1,157 +1,171 @@
 #include <vector>
 #include <list>
-#include <iostream>
 #include <cassert>
-namespace example_01 { // -> initialize of std::vector
+namespace example_01 { // -> initialize std::vector
 void run() {
-    std::vector<int> const v1{ 1, 2, 3, 4, 5 };
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v1));
-    
+    {
+    std::vector<int> const v{ 1, 2, 3, 4, 5 };
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
     int constexpr const arr[] = { 1, 2, 3, 4, 5 };
-    std::vector<int> const v2{ arr, arr + 5 };
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v2));
-    
+    std::vector<int> const v{ arr, arr + 5 };
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
     std::list<int> const l{ 1, 2, 3, 4, 5 };
-    std::vector<int> const v3{ l.begin(), l.end() };
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v3));
-    
-    std::vector<int> const v4(5, 1);
-    assert((std::vector<int>{ 1, 1, 1, 1, 1 } == v4));
-    
-    std::cout << "[ok]";
+    std::vector<int> const v(std::cbegin(l), std::cend(l));
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
+    std::vector<int> const v(5, 1);
+    assert((std::vector<int>{ 1, 1, 1, 1, 1 } == v));
+    }
 }    
 } // example_01
 
 
+
 #include <vector>
 #include <string>
-#include <iostream>
 #include <cassert>
 namespace example_02 { // -> modify std::vector
 void run() {
-    std::vector<int> v1{ 1, 2, 3, 4, 5 };
-    v1.push_back(6);
-    assert((std::vector<int>{ 1, 2, 3, 4, 5, 6 } == v1));
-    
-    v1.pop_back();
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v1));
-    
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    v.push_back(6);
+    assert((std::vector<int>{ 1, 2, 3, 4, 5, 6} == v));
+
+    v.pop_back();
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
     int constexpr const arr[] = { 1, 2, 3, 4, 5 };
-    std::vector<int> v2{};
-    v2.insert(v2.begin(), arr, arr + 5);
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v2));
-    
+    std::vector<int> v{};
+    v.insert(std::begin(v), arr, arr + 5);
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
     using namespace std::string_literals;
     struct foo final {
         int a;
         double b;
-        std::string c;
-        
-        foo(int const a, double const b, std::string const& c)
-            : a{a}, b{b}, c{c} {}
+        std::string c;        
+
+        foo (int const a, double const b, std::string const& c)
+            : a(a), b(b), c(c) {}
+
+        bool operator==(foo const& other) const { return other.a == a and std::abs(other.b - b) < 0.001 and other.c == c; }
     };
-    std::vector<foo> v3{};
-    v3.emplace_back(1, 1.0, "one"s);
-    assert((
-        not v3.empty() and 1 == v3.size() 
-        and 1 == v3.begin()->a and "one"s == v3.begin()->c));
-    
-    v3.emplace(v3.begin(), 2, 2.0, "two"s);
-    assert((
-        not v3.empty() and 2 == v3.size()
-        and 2 == v3.begin()->a and "two"s == v3.begin()->c));
-        
-    std::cout << "[ok]";
+    std::vector<foo> v{};
+    v.emplace_back(1, 1.0, "one"s);
+    assert((std::vector<foo>{ { 1, 1.0, "one"s} } == v));
+
+    v.emplace(std::begin(v), 2, 2.0, "two"s);
+    assert((std::vector<foo>{ { 2, 2.0, "two"s }, { 1, 1.0, "one"s} } == v));
+    }
 }
 } // example_02
 
 
 #include <vector>
-#include <iostream>
 #include <cassert>
-namespace example_03 { // modify the whole std::vector
+namespace example_03 { // -> modify the whole std::vector
 void run() {
+    {
     std::vector<int> const v1{ 1, 2, 3, 4, 5 };
     std::vector<int> v2{ 10, 20, 30 };
     v2 = v1;
-    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v2));
-    
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v1));
+    }
+
+    {
     int constexpr const arr[] = { 1, 2, 3, 4, 5 };
-    std::vector<int> v3{};
-    v3.assign(arr, arr + 5);
-    assert((std::vector<int> { 1, 2, 3, 4, 5 } == v3));
-    
-    std::vector<int> v4{ 1, 2, 3, 4, 5 };
-    std::vector<int> v5{ 10, 20, 30 };
-    v4.swap(v5);
-    assert((std::vector<int>{ 10, 20, 30 } == v4
-        and std::vector<int>{ 1, 2, 3, 4, 5 } == v5));
-        
-    std::vector<int> v6{ 1, 2, 3, 4, 5 };
-    v6.clear();
-    assert(v6.empty());
-    
-    std::vector<int> v7{ 1, 2, 3, 4, 5 };
-    v7.erase(v7.begin() + 2, v7.begin() + 4);
-    assert((std::vector<int>{ 1, 2, 5 } == v7));
-    
-    std::cout << "[ok]";
-}    
+    std::vector<int> v{};
+    v.assign(arr, arr + 5);
+    assert((std::vector<int>{ 1, 2, 3, 4, 5 } == v));
+    }
+
+    {
+    std::vector<int> v1{ 1, 2, 3, 4, 5 };
+    std::vector<int> v2{ 10, 20, 30 };
+    v1.swap(v2);
+    assert((std::vector<int>{ 10, 20, 30 } == v1
+        and std::vector<int>{ 1, 2, 3, 4, 5 } == v2));
+    }
+
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    v.clear();
+    assert((v.empty()));
+    }
+
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    v.erase(std::begin(v) + 2, std::begin(v) + 4);
+    assert((std::vector<int>{ 1, 2, 5 } == v));
+    }
+}
 } // example_03
 
 
 #include <vector>
-#include <iostream>
+#include <cassert>
 namespace example_04 { // -> std::vector with C-like API
 
 void process([[maybe_unused]] int const * const arr, [[maybe_unused]] int const size) {} // C-like function
 
 void run() {
     std::vector<int> v{ 1, 2, 3, 4, 5 };
-    
+
     process(v.data(), static_cast<int>(v.size()));
     process(&v[0], static_cast<int>(v.size()));
     process(&v.front(), static_cast<int>(v.size()));
-    process(&*v.begin(), static_cast<int>(v.size()));
-
-    std::cout << "[ok]";
+    process(&*std::begin(v), static_cast<int>(v.size()));
+    process(&*std::cbegin(v), static_cast<int>(v.size()));
 }
 } // example_04
 
 
 #include <vector>
-#include <iostream>
 #include <cassert>
-namespace example_05 { // -> reserve space for std::vector
+namespace example_05 { // -> space allocation/deallocation for std::vector
 void run() {
-    std::vector<int> v1{ 1, 2, 3, 4, 5 };
-    v1.reserve(10);
-    assert(10 == v1.capacity());
-    v1.shrink_to_fit();
-    assert(v1.size() == v1.capacity());
-    
-    std::vector<int> v2{ 1, 2, 3, 4, 5 };
-    std::vector<int>{}.swap(v2);
-    assert(v2.empty() and 0 == v2.capacity());
-   
-    std::vector<int> v3{ 1, 2, 3, 4, 5 };
-    v3.reserve(10);
-    assert(10 == v3.capacity());
-    v3.clear();
-    assert(v3.empty() and 10 == v3.capacity());
-    
-    std::cout << "[ok]";
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    v.reserve(10);
+    assert((10 == v.capacity()));
+    }
+
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    std::vector<int>{}.swap(v);
+    assert((v.empty() and 0 == v.capacity()));
+    }
+
+    {
+    std::vector<int> v{ 1, 2, 3, 4, 5 };
+    v.reserve(10);
+    assert((not v.empty() and 10 == v.capacity()));
+    v.clear();
+    assert((v.empty() and 10 == v.capacity()));
+    }
 }
-    
 } // example_05
 
 
+#include <iostream>
 int main() {
-    std::cout << "example_01 => " << std::endl; example_01::run(); std::cout << std::endl << std::endl;
-    std::cout << "example_02 => " << std::endl; example_02::run(); std::cout << std::endl << std::endl;
-    std::cout << "example_03 => " << std::endl; example_03::run(); std::cout << std::endl << std::endl;
-    std::cout << "example_04 => " << std::endl; example_04::run(); std::cout << std::endl << std::endl;
-    std::cout << "example_05 => " << std::endl; example_05::run(); std::cout << std::endl << std::endl;
-    
-	return 0;
+    std::cout << "[ok] - example_01 => " << std::endl; example_01::run(); std::cout << std::endl << std::endl;
+    std::cout << "[ok] - example_02 => " << std::endl; example_02::run(); std::cout << std::endl << std::endl;
+    std::cout << "[ok] - example_03 => " << std::endl; example_03::run(); std::cout << std::endl << std::endl;
+    std::cout << "[ok] - example_04 => " << std::endl; example_04::run(); std::cout << std::endl << std::endl;
+    std::cout << "[ok] - example_05 => " << std::endl; example_05::run(); std::cout << std::endl << std::endl;
+
+    return 0;
 }
