c72edf7e992d7872c80fc79295855a88fb3a30be
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-12-31 21:31:01 +0300

    [m_noback-object_design_style_guide][ch 03] add an example from subchapter 3.6

---
+++ books/tech/software_design/m_noback-object_design_style_guide/data/ch_03-creating_other_objs/10-extract_new_obj_for_composite_values/main.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <stdexcept>
+#include <cassert>
+namespace workspace {
+
+
+class Amount {
+public:
+	Amount(const int value) : value_(value) {}
+
+private:
+	const int value_;
+};
+
+
+class Currency {
+public:
+	Currency(const std::string& value) : value_(value) {}
+
+private:
+	const std::string value_;
+};
+
+class Money {
+public:
+	static Money create(const int amount, const std::string& currency) {
+		return Money(Amount(amount), Currency(currency));
+	}
+
+private:
+    Money(const Amount& amount, const Currency& currency)
+        : amount_(amount), currency_(currency) {}
+
+    const Amount amount_;
+    const Currency currency_;
+};
+
+class Product {
+public:
+    Product(const Money& money) : money_(money) {}
+
+private:
+    Money money_;
+};
+
+class Converter {
+public:
+    Money convert(const Money& money) const {
+        return Money::create(100, "EUR");
+    }
+};
+
+
+void run() {
+    {
+        Money::create(100, "USD");
+    }
+    
+    {
+        Product(Money::create(100, "USD"));
+    }    
+    
+    {
+        Converter().convert(Money::create(100, "USD"));
+    }
+}
+
+} // workspace
+
+
+int main() {
+    workspace::run();
+    return 0;
+}
