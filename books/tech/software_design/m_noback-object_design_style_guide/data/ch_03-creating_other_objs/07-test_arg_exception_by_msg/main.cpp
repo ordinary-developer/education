c2047a8ba54768f0395807632832aa58facb906c
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-12-31 19:15:35 +0300

    [m_noback-object_design_style_guide][ch 03] add an example for subchapter 3.4

---
+++ books/tech/software_design/m_noback-object_design_style_guide/data/ch_03-creating_other_objs/07-test_arg_exception_by_msg/main.cpp
@@ -0,0 +1,82 @@
+#include <string>
+#include <stdexcept>
+#include <cassert>
+namespace workspace {
+
+class Coords {
+public:
+    Coords(const double latitude, const double longitude)
+        : latitude_(latitude), longitude_(longitude)
+    {
+        const bool isLatitudeInvalid = latitude_ > 90 || latitude_ < -90;
+        const bool isLongitudeInvalid = longitude_ > 180 || longitude_ < -180;
+
+        if (isLatitudeInvalid && isLongitudeInvalid)
+            throw std::invalid_argument(
+                "both the latitude and longitude are invalid");
+        if (isLatitudeInvalid)
+            throw std::invalid_argument(
+                "the latitude is invalid");
+        if (isLongitudeInvalid)
+            throw std::invalid_argument(
+                "the longitude is invalid");
+    }
+private:
+    const double latitude_;
+    const double longitude_;
+};
+
+void run() {
+    {
+        bool wasException = false;
+
+        try {
+            Coords(90.1, 180.1);
+        }
+        catch (const std::invalid_argument& e) { 
+            assert((std::string("both the latitude and longitude are invalid") == std::string(e.what())));
+            assert((wasException = true)); 
+        }
+        catch (...) { assert((false)); }
+
+        assert((wasException));
+    }
+
+    {
+        bool wasException = false;
+
+        try {
+            Coords(90.1, 0);
+        }
+        catch (const std::invalid_argument& e) { 
+            assert((std::string("the latitude is invalid") == std::string(e.what())));
+            assert((wasException = true)); 
+        }
+        catch (...) { assert((false)); }
+
+        assert((wasException));
+    }
+
+    {
+        bool wasException = true;
+
+        try {
+            Coords(0, 180.1);
+        }
+        catch (const std::invalid_argument& e) { 
+            assert((std::string("the longitude is invalid") == std::string(e.what())));
+            assert((wasException = true)); 
+        }
+        catch (...) { assert((false)); }
+
+        assert((wasException));
+    }
+}
+
+} // workspace
+
+
+int main() {
+    workspace::run();
+    return 0;
+}
