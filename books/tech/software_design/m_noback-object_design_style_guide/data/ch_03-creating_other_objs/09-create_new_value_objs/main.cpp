17a3bc910834f7fafbf3008a2c81fb4fd9988cd1
ordinary-developer <merely.ordinary.developer@gmail.com>
2020-12-31 19:50:11 +0300

    [m_noback-object_design_style_guide][ch 03] add an exercise impl from subchapter 3.5

---
+++ books/tech/software_design/m_noback-object_design_style_guide/data/ch_03-creating_other_objs/09-create_new_value_objs/main.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <stdexcept>
+#include <cassert>
+namespace workspace {
+
+class CountryCode {
+public:
+    explicit CountryCode(const std::string& code) : code_(code) {
+        if (KNOWN_CODES[0] != code_ && KNOWN_CODES[1] != code_)        
+            throw std::invalid_argument(std::string("unknown country code ").append(code));
+    }
+
+private:
+    static const std::string KNOWN_CODES[2];
+    const std::string code_;
+};
+const std::string CountryCode::KNOWN_CODES[2] = { "NL", "GB" };
+
+void run() {
+    CountryCode("NL");
+    CountryCode("GB");
+    
+    {
+        bool wasException = false;
+    
+        const std::string invalidCode = "ABC";
+    
+        try {
+            CountryCode countryCode(invalidCode);    
+        }
+        catch (const std::invalid_argument& e) {        
+            assert((std::string("unknown country code ").append(invalidCode) 
+                == std::string(e.what())));
+            assert((wasException = true));
+        }       
+        catch (...) { assert((false)); }
+        
+        assert((wasException));
+    }
+}
+
+} // workspace
+
+
+int main() {
+    workspace::run();
+    return 0;
+}
