8c381e712d5e6d5d221c354603e2e9e352dfaf65
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-02-13 20:33:22 +0300

    [i_cukic-func_progr_in_cpp][ch 01] slighlty rewrite line counting with stl_transform

--- books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_01-intro_to_fp/03-line_num_calc_with_stl_transform/main.cpp
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_01-intro_to_fp/03-line_num_calc_with_stl_transform/main.cpp
@@ -1,30 +1,72 @@
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <fstream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <algorithm>
-#include <iterator>
+namespace test_01 { // declaratively read from a file (with a normal func for std::transform)
 
+int countLinesImpl(std::string const& file) {
+    std::ifstream in{file};
 
-int count_lines(std::string const& filename) {
-    std::ifstream in{filename};
     return std::count(
         std::istreambuf_iterator<char>{in},
         std::istreambuf_iterator<char>{},
         '\n');
 }
 
-std::vector<int> count_lines_in_files(std::vector<std::string> const& files) {
-    std::vector<int> results(files.size());
-    std::transform(std::cbegin(files), std::cend(files), results.begin(), count_lines);
+std::vector<int> countLines(std::vector<std::string> const& files) {
+    std::vector<int> ret(files.size());
 
-    return results;    
+    std::transform(std::cbegin(files), std::cend(files), ret.begin(), countLinesImpl);
+
+    return ret;
 }
+
+void run() {
+    for (auto const cnt: countLines( { "main.cpp", "main.cpp", "main.cpp" }))
+        std::cout << "line count: " << cnt << std::endl;
+}   
+} // test_01
+
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <string>
+#include <vector>
+namespace test_02 { // declaratively read from a file (with a lambda func for std::transform)
+
+std::vector<int> countLines(std::vector<std::string> const& files) {
+    std::vector<int> ret(files.size());
+
+    std::transform(
+        std::cbegin(files),
+        std::cend(files),
+        std::begin(ret),
+        [](auto const& file) {
+            std::ifstream in{file};
+
+            return std::count(
+                std::istreambuf_iterator<char>{in},
+                std::istreambuf_iterator<char>{},
+                '\n');
+        });
+
+    return ret;
+}
+
+void run() {
+    for (auto const cnt: countLines( { "main.cpp", "main.cpp", "main.cpp" }))
+        std::cout << "line count: " << cnt << std::endl;
+}    
+} // test_02
 
 
 int main() {
-    for (auto const line_count : count_lines_in_files( { "main.cpp" }))
-        std::cout << line_count << " line(s)\n";
+    std::cout << "test 01 => [ok]" << std::endl; test_01::run(); std::cout << std::endl;
+    std::cout << "test 02 => [ok]" << std::endl; test_02::run(); std::cout << std::endl;
 
     return 0;
 }
