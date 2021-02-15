dd1728ffef79b73cf1a8caed6baf25e7c328ed29
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-02-15 20:42:23 +0300

    [i_cukic-func_prgr_in_cpp][ch 01] slightly rewrite a more elaborate example with ranges

--- books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_01-intro_to_fp/06-line_num_calc_with_2_transforms/main.cpp
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_01-intro_to_fp/06-line_num_calc_with_2_transforms/main.cpp
@@ -1,38 +1,42 @@
 #include <algorithm>
-#include <ranges>
-#include <vector>
-#include <string>
-#include <sstream>
 #include <iostream>
 #include <iterator>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <vector>
+namespace test {
+    
+std::istringstream open_str(std::string const& str) { return std::istringstream{str}; }
 
-std::istringstream open_str(std::string const& str) {
-    return std::istringstream{str};
-}
-
-
-int count_lines(std::istringstream strstream) {        
+int count_lines(std::istringstream strstream) {
     return std::count(
-        std::istreambuf_iterator<char>(strstream),
-        std::istreambuf_iterator<char>(),
+        std::istreambuf_iterator<char>{strstream},
+        std::istreambuf_iterator<char>{},
         '\n');
 }
 
-std::vector<int> count_lines_in_files(std::vector<std::string> const& strs) {
-    auto const& tmp_vals = strs
+std::vector<int> countLines(std::vector<std::string> const& strs) {
+    std::vector<int> ret{};  
+    
+    auto const& vals = strs
         | std::ranges::views::transform(open_str)
-        | std::ranges::views::transform(count_lines);        
+        | std::ranges::views::transform(count_lines);
+        
+    std::copy(std::cbegin(vals), std::cend(vals), std::back_inserter(ret));        
     
-    std::vector<int> cnts{};
-    std::copy(std::cbegin(tmp_vals), std::cend(tmp_vals), std::back_inserter(cnts));
-    
-    return cnts;
+    return ret;
 }
 
+void run() {
+    for (auto const cnt: countLines( { "1\n2\n3", "1\n2\n3", "1\n2\n3" } ))
+        std::cout << "line count: " << cnt << std::endl;
+}
+} // test
 
-int main() {    
-    for (auto const line_count : count_lines_in_files( { "1\n2\n3" }))
-       std::cout << line_count << " line(s)\n";
+
+int main() {
+    std::cout << "test => [ok]" << std::endl; test::run(); std::cout << std::endl;
 
     return 0;
 }
