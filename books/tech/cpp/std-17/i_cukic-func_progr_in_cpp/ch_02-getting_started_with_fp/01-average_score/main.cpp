cc461e27cb03c28d5a99cad342f067ef03289f69
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-02-17 21:29:11 +0300

    [i_cukic-func_progr_in_cpp][ch 02] slightly rewrite the code for the average_score function

--- books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_02-getting_started_with_fp/01-average_score/main.cpp
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_02-getting_started_with_fp/01-average_score/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <numeric>
+// calculating the average score imperatively
 #include <vector>
-#include <execution>
+#include <cassert>
+#include <cmath>
+namespace test_01 {
 
-double average_score_imperative(std::vector<int> const& scores) {
-    int sum = 0;
+double average_score(std::vector<int> const& scores) {
+    int sum{0};
 
     for (int score : scores)
         sum += score;
@@ -12,31 +13,86 @@ double average_score_imperative(std::vector<int> const& scores) {
     return sum / (double)scores.size();
 }
 
+void run() {
+    double const val = average_score({ 1, 2, 3, 4, 5 });
+
+    assert(std::abs(3. - val) < 0.001);
+}
+} // test_01
+
+
+// calculating the average score functionally
+#include <numeric>
+#include <vector>
+#include <cassert>
+#include <cmath>
+namespace test_02 {
+
 double average_score(std::vector<int> const& scores) {
-    return
+    return 
         std::accumulate(std::cbegin(scores), std::cend(scores), 0)
-        /
-        (double)scores.size();
+        / (double)scores.size();
 }
 
-double average_score_par(std::vector<int> const& scores) {
-    return 
-        std::reduce(std::execution::par, std::cbegin(scores), std::cend(scores), 0)
-        /
-        (double)scores.size();
+void run() {
+    double const val = average_score({ 1, 2, 3, 4, 5 });
+
+    assert(std::abs(3. - val) < 0.001);
 }
+
+} // test_02
+
+
+// calculating the average with parallel execution
+// (at least specify that execution should be parallelized)
+#include <execution>
+#include <numeric>
+#include <vector>
+#include <cassert>
+#include <cmath>
+namespace test_03 {
+
+double average_score(std::vector<int> const& scores) {
+    return
+        std::reduce(std::execution::par, std::cbegin(scores), std::cend(scores), 0)
+        / (double)scores.size();
+}
+
+void run() {
+    double const val = average_score({ 1, 2, 3, 4, 5 });
+
+    assert(std::abs(3. - val) < 0.001);
+}
+} // test_03
+
+
+// calculating the product of all scores
+#include <functional>
+#include <numeric>
+#include <vector>
+#include <cmath>
+#include <cassert>
+namespace test_04 {
 
 double scores_product(std::vector<int> const& scores) {
     return std::accumulate(
-        std::cbegin(scores), std::cend(scores), 1, std::multiplies<int>{});
+        std::cbegin(scores), 
+        std::cend(scores), 
+        1, 
+        std::multiplies<int>{});
 }
 
+void run() { assert(120 == scores_product({ 1, 2, 3, 4, 5 })); }
 
-int main(int, char**) {
-    std::cout << average_score_imperative({1, 2, 3, 4}) << std::endl;
-    std::cout << average_score({1, 2, 3, 4}) << std::endl;
-    std::cout << average_score_par({1, 2, 3, 4}) << std::endl;
-    std::cout << scores_product({1, 2, 3, 4}) << std::endl;
-    
+} // test_04
+
+
+#include <iostream>
+int main() {
+    std::cout << "test_01 => [ok]" << std::endl; test_01::run(); std::cout << std::endl;
+    std::cout << "test_02 => [ok]" << std::endl; test_02::run(); std::cout << std::endl;
+    std::cout << "test_03 => [ok]" << std::endl; test_03::run(); std::cout << std::endl;
+    std::cout << "test_04 => [ok]" << std::endl; test_04::run(); std::cout << std::endl;
+
     return 0;
 }
