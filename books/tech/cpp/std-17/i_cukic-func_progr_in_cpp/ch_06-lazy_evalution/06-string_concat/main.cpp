da785ceeefc8d50e2b14f939ebb5f9824aa15884
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-03-03 21:10:29 +0300

    [i_cukic-func_progr_in_cpp][ch 06] add an example for string concatenation

---
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_06-lazy_evalution/06-string_concat/main.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <tuple>
+
+#include <cassert>
+namespace test {
+
+template <typename... Strings>
+class lazy_string_concat_helper;
+
+template <typename LastString, typename... Strings>
+class lazy_string_concat_helper<LastString, Strings...> {
+private:
+    LastString data;
+    lazy_string_concat_helper<Strings...> tail;
+
+public:
+    lazy_string_concat_helper(
+        LastString data,
+        lazy_string_concat_helper<Strings...> tail
+    ) : data{data}, tail{tail} {}
+
+    int size() const { return data.size() + tail.size(); }
+
+    template <typename It>
+    void save(It end) const {
+        auto const begin = end - data.size();
+        std::copy(std::cbegin(data), std::cend(data), begin);
+        tail.save(begin);
+    }
+
+    operator std::string() const {
+        std::string result(size(), '\0');
+        save(result.end());
+        return result;
+    }
+
+    lazy_string_concat_helper<std::string, LastString, Strings...>
+    operator + (std::string const& other) const {
+        return lazy_string_concat_helper<std::string, LastString, Strings...>(other, *this);
+    }
+};
+
+template <>
+class lazy_string_concat_helper<> {
+public:
+    lazy_string_concat_helper() {}
+
+    int size() const { return 0; }
+
+    template <typename It> void save(It) const {}
+
+    lazy_string_concat_helper<std::string>
+    operator + (std::string const& other) const {
+        return lazy_string_concat_helper<std::string>(other, *this);
+    }
+};
+
+lazy_string_concat_helper<> lazy_concat;
+
+void run() {
+    std::string name{ "Jane" }, surname{ "Smith" };
+    
+    std::string const fullname = lazy_concat + surname + ", " + name;
+
+    assert("Smith, Jane" == fullname);
+
+    std::cout << fullname << std::endl;
+}
+
+} // test
+
+
+#include <iostream>
+int main() {
+    std::cout << "test => [ok]" << std::endl; test::run(); std::cout << std::endl;
+
+    return 0;
+}
