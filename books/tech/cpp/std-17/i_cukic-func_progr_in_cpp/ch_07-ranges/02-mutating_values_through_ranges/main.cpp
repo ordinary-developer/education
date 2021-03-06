c84160244030e4a52dd8355532ee1784ce0b8ed1
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-03-06 14:40:54 +0300

    [i_cukic-func_progr_in_cpp][ch 07] update the 2-nd example for std::ranges

--- books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_07-ranges/02-mutating_values_through_ranges/main.cpp
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_07-ranges/02-mutating_values_through_ranges/main.cpp
@@ -5,20 +5,18 @@
 #include <type_traits>
 #include <vector>
 #include <cassert>
-namespace test {    
-    
-namespace rv = std::ranges::views;
-namespace rg = std::ranges;
+namespace test {
     
 void run() {
-    std::vector<std::string> src_words{ "The", "brown", "fox", "jumped", "over", "a", "dog" };
+    std::vector<std::string> words_raw{ "The", "brown", "fox", "jumped", "over", "a", "dog" };
     
-    auto words = std::views::all(src_words);
+    auto words = std::views::all(words_raw);
     std::ranges::sort(words);
     std::ranges::unique(words);
     
     std::vector<std::decay_t<std::ranges::range_value_t<decltype(words)>>> const words_as_vec{ std::cbegin(words), std::cend(words) };
-    assert((std::vector<std::string>{ "The", "a", "brown", "dog", "fox", "jumped", "over" } == words_as_vec));   
+    assert((std::vector<std::string>{ "The", "a", "brown", "dog", "fox", "jumped", "over" } == words_raw));
+    assert((std::vector<std::string>{ "The", "a", "brown", "dog", "fox", "jumped", "over" } == words_as_vec));
 }
     
 } // test
@@ -29,4 +27,4 @@ int main() {
     std::cout << "test => [ok]" << std::endl; test::run(); std::cout << std::endl;   
         
     return 0;
-}
+} 
