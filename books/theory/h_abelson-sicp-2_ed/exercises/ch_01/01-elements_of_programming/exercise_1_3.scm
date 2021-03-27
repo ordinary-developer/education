80329bd4ce8c3453c8038b8c181f89571a6b3543
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-03-27 21:30:20 +0300

    [h_abelson_et_al-sicp-2_ed][ch 01] add another portion of solutions for exercise 1.3

--- books/theory/h_abelson-sicp-2_ed/exercises/ch_01/01-elements_of_programming/exercise_1_3.scm
+++ books/theory/h_abelson-sicp-2_ed/exercises/ch_01/01-elements_of_programming/exercise_1_3.scm
@@ -209,3 +209,69 @@ yet another solution
    
 
 
+;; ----------------
+;; another solution
+(define (sum_of_largest param1 param2 param3)
+    (define (sqr param) (* param param))
+    (define (sum_of_sqrs param1 param2) (+ (sqr param1) (sqr param2)))
+    
+    (cond 
+         ((and (>= param1 param3)(>= param2 param3)) (sum_of_sqrs param1 param2))
+         ((and (>= param1 param2)(>= param3 param2)) (sum_of_sqrs param1 param3))
+         (else (sum_of_sqrs param2 param3))))
+
+
+(define ret (=
+    13 
+    (sum_of_largest 1 2 3)
+    (sum_of_largest 1 3 2)
+    (sum_of_largest 2 1 3)
+    (sum_of_largest 2 3 1)
+    (sum_of_largest 3 1 2)
+    (sum_of_largest 3 2 1)))
+(display ret) (newline)
+
+
+;; ----------------
+;; another solution
+(define (sum_of_largest param1 param2 param3)
+    (define (sqr param) (* param param))
+    (define (sum_of_sqrs param1 param2) (+ (sqr param1) (sqr param2)))
+    
+    (cond
+        ((< param1 param2) (sum_of_sqrs param2 (max param1 param3)))
+        (else (sum_of_sqrs param1 (max param2 param3)))))
+
+
+(define ret (=
+    13 
+    (sum_of_largest 1 2 3)
+    (sum_of_largest 1 3 2)
+    (sum_of_largest 2 1 3)
+    (sum_of_largest 2 3 1)
+    (sum_of_largest 3 1 2)
+    (sum_of_largest 3 2 1)))
+(display ret) (newline)
+
+
+
+;; ----------------
+;; another solution
+;; applicable only for non-negative numbers
+(define (sum_of_largest param1 param2 param3)
+    (define (sqr param) (* param param))
+    (define (sum_of_sqrs param1 param2) (+ (sqr param1) (sqr param2)))
+
+    (max (sum_of_sqrs param1 param2) (sum_of_sqrs param2 param3) (sum_of_sqrs param1 param3)))
+
+
+
+(define ret (=
+    13 
+    (sum_of_largest 1 2 3)
+    (sum_of_largest 1 3 2)
+    (sum_of_largest 2 1 3)
+    (sum_of_largest 2 3 1)
+    (sum_of_largest 3 1 2)
+    (sum_of_largest 3 2 1)))
+(display ret) (newline)
