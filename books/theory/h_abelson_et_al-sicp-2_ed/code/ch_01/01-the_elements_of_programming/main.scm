60c065e6ab9cf2cf62615eae4d7e7f526e4f6f24
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-04-16 21:19:34 +0300

    [h_abelson_et_al-sicp-2_ed][ch_01] slightly change the "expressions" section

--- books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/01-the_elements_of_programming/main.scm
+++ books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/01-the_elements_of_programming/main.scm
@@ -1,27 +1,26 @@
 ;; expressions
-(display "[expressions] =>") (newline)
-(display 486)
+(display "=> expressions") (newline)
+(display 486) (newline)
+
 (display (+ 137 349)) (newline)
+(display (- 1000 334)) (newline)
 (display (* 5 99)) (newline)
 (display (/ 10 5)) (newline)
 (display (+ 2.7 10)) (newline)
 
-(newline)
 (display (+ 21 35 12 7)) (newline)
 (display (* 25 4 12)) (newline)
 
-(newline)
 (display (+ (* 3 5) (- 10 6))) (newline)
 
-(newline)
 (display (+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))) (newline)
 (display 
 (+ (* 3
       (+ (* 2 4)
          (+ 3 5)))
-   (+ (- 10 7)
-      6))
-) (newline)           
+   (+ (- 10 7)      
+       6))
+) (newline)         
 
 
 ;; naming and the environment
