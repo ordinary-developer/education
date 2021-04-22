c6377c8c6615e031f52c3444310606560025937d
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-04-22 21:39:44 +0300

    [h_abelson_et_al-sicp-2_ed][ch 01] change the "Newton's method" section

--- books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/01-the_elements_of_programming/main.scm
+++ books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/01-the_elements_of_programming/main.scm
@@ -108,29 +108,27 @@
 
 
 ;; example: square roots by Newton's Method
-(newline)(newline)
-(display "[example: square roots by Newton's Method] =>") (newline)
-(define (sqrt x) (sqrt_iter 1.0 x))
+(display "example: square roots by Newton's Method =>") (newline)
+(define (square x) (* x x))
+(define (sqrt x)
+    (define (sqrt_iter guess x)
+        (if (good_enough? guess x)
+            guess
+            (sqrt_iter (improve guess x) x)))
 
-(define (sqrt_iter guess x)
-    (if (good_enough? guess x)
-        guess
-        (sqrt_iter (improve guess x) x)))
+    (define (good_enough? guess x)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess x)
+        (average guess (/ x guess)))
+    (define (average x y)
+        (/ (+ x y) 2))
 
-(define (good_enough? guess x)
-    (< (abs (- (* guess guess) x)) 0.001))
-
-(define (improve guess x)
-    (average guess (/ x guess)))
-
-(define (average x y)
-    (/ (+ x y) 2))
-
+    (sqrt_iter 1.0 x) 
+)
 
 (display (sqrt 9)) (newline)
-(display (sqrt (+ 100 37))) (newline)
 (display (sqrt (+ (sqrt 2) (sqrt 3)))) (newline)
-(display (* (sqrt 1000) (sqrt 1000))) (newline)
+(display (square (sqrt 1000))) (newline)
 
 
 ;; procedures as black-box abstractions
