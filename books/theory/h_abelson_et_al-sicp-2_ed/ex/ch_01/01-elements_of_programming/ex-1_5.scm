7a16346790cf2fc5bdcc722c2af06705ae70ae50
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-04-23 20:58:54 +0300

    [h_abelson_et_al-sicp-2_ed][ch 01] add a solution to exercise 1.5

---
+++ books/theory/h_abelson_et_al-sicp-2_ed/ex/ch_01/01-elements_of_programming/ex-1_5.scm
@@ -0,0 +1,44 @@
+;; [exercise] =>
+;; Ben Bitdiddle has invetend a test to determine
+;; whter the interpreter he is faced with
+;; is using applicative-order evalution or normal-order evaluation.
+;; He defines the following two procedures:
+;; (define (p) (p))
+;; (define (test x y)
+;;     (if (= x 0) 0 y))
+;;
+;; Then he evaluates the expression
+;; (test 0 (p))
+;;
+;; What behavior will Ben observe with an interpreter
+;; that uses applicative-order evaluation?
+;; What behavior will he observe with an interpreter
+;; that uses normal-order evalution?
+;; Explain your answer.
+;;
+;; (Assume that the evaluation rule for the special form 'if'
+;   is the same wether the interpreter is using normal or applicative order:
+;;  The predicate expression is evaluated first,
+;;  and the result determines whether to evaluate the consequent
+;;  or the alternative expression.)
+
+
+;; [solution] =>
+;; # Applicative order
+;; (test 0 (p))
+;; will become
+;; (if (= 0 0) 0 (p))
+;; after that it will become
+;; (if (= 0 0) 0 (p))
+;; and so on.
+;; So this evaluation will never be completed.
+
+;; # Normal order
+;; (test 0 (p))
+;; will become
+;; (if (= 0 0) 0 (p))
+;; after that according to normal-order evaluation rules it will become
+;; (if (#t) 0 (p))
+;; and after that it will become
+;; 0
+;; So in this case the result of (test 0 (p)) will be '0'
