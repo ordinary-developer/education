8749250c7d4a24f478ecb8fc3e16254048ade112
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-02 21:48:07 +0300

    [h_abelson_et_al-sicp-2_ed][ch 01] slightly change ex 1.2

--- books/theory/h_abelson_et_al-sicp-2_ed/ex/ch_01/01-elements_of_programming/ex-1_2.scm
+++ books/theory/h_abelson_et_al-sicp-2_ed/ex/ch_01/01-elements_of_programming/ex-1_2.scm
@@ -1,10 +1,14 @@
-;; translate the following expression into prefix form:
-;;
-;; 5 + 4 + (2 - (3 - (6 + 4/5)))
-;; ----------------------------
-;;       3(6 - 2)(2 - 7)
-;;
+; [exercise] =>
+; translate the following expression into prefix form:
+;
+; 5 + 4 + (2 - (3 - (6 + 4/5)))
+; ----------------------------
+;       3(6 - 2)(2 - 7)
+;
 
+
+; -------------
+; [solution] =>
 (define result 
     (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
        (* 3 (- 6 2) (- 2 7)))
