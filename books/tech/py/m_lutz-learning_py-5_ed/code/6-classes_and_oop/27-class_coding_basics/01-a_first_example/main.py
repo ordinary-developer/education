87dd317bf645e393b5237ecba2805fc20ef4d2d2
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-03 21:54:56 +0300

    [m_lutz-learning_py-5_ed][ch 27] slightly change the first example

--- books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/27-class_coding_basics/01-a_first_example/main.py
+++ books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/27-class_coding_basics/01-a_first_example/main.py
@@ -1,35 +1,27 @@
-class FirstClass:
-    var_in_all_objects = 0
-    def setdata(self, value):
-        self.data = value
-    def display(self):
-        print(self.data)
-    def static_method():
-        print('static_method')
+# classes generate multiple instance objects
+def example_01(): 
+    print("example 01 (classes generate multiple instance objects) =>")
 
-      
-if __name__ == '__main__':
+    class FirstClass:
+        def setdata(self, value):
+            self.data = value
+        def display(self):
+            print(self.data)
+
     x = FirstClass()
     y = FirstClass()
+
     x.setdata("King Arthur")
     y.setdata(3.14159)
-
-    FirstClass.var_in_all_objects = 'abc'
-    x.var_in_all_objects = 15
-    print(FirstClass.var_in_all_objects)
-    print(x.var_in_all_objects)
-    print(y.var_in_all_objects)
-
-    x.var_in_all_objects = 'str'
-    print(x.var_in_all_objects)
 
     x.display()
     y.display()
 
-    FirstClass.static_method()
-
-    x.data = "new value"
+    x.data = "New value"
     x.display()
-    
+
     x.anothername = "spam"
-    print(x.anothername)
+
+
+if __name__ == '__main__':
+    example_01()
