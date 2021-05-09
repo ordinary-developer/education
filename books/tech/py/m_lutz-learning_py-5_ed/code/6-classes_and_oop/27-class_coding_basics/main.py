d605c323584d142f0acde2ecfb446b6a596b3f04
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-09 21:59:21 +0300

    [m_lutz-learning_py-5_ed][ch 27] add code for the "built-in operators" section

--- books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/27-class_coding_basics/main.py
+++ books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/27-class_coding_basics/main.py
@@ -42,6 +42,44 @@ def example_02():
     z.display()
 
 
+# the "classes can intercept Python operators" section
+def example_03():
+    print("example 03 (classes can intercept Python operators) =>")
+
+    class FirstClass:
+        def setdata(self, value):
+            self.data = value
+        def display(self):
+            print(self.data)
+
+    class SecondClass(FirstClass):
+        def display(self):
+            print('Current value = "%s"' % self.data)
+
+    class ThirdClass(SecondClass):
+        def __init__(self, value):
+            self.data = value
+        def __add__(self, other):
+            return ThirdClass(self.data + other)
+        def __str__(self):
+            return '[ThirdClass: %s]' % self.data
+        def mul(self, other):
+            self.data *= other
+
+    a = ThirdClass('abc')
+    a.display()
+    print(a)
+
+    b = a + 'xyz'
+    b.display()
+    print(b)
+    a.mul(3)
+    print(a)
+
+
+
+
 if __name__ == '__main__':
     example_01()
     example_02()
+    example_03()
