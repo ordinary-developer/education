ce0948415e8d8541ee5da92ad31aa21591e2b1d5
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-13 21:55:38 +0300

    [h_abelson_et_al-sicp-2_ed][ch 01] the the "linear recursion" section in subchapter 02

--- books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/02-procedures/main.scm
+++ books/theory/h_abelson_et_al-sicp-2_ed/code/ch_01/02-procedures/main.scm
@@ -1,13 +1,11 @@
-;; linear recursion and iteration
-(display "[linear recursion and iteration] =>") (newline)
-
+;; the "linear recursion and iteration" section
+(display "linear recursion and iteration =>") (newline)
 (define (factorial n)
     (if (= n 1)
         1
-        (* n (factorial (- n 1)))))   
-(display (factorial 3)) (newline)
-(display (factorial 4)) (newline)
-(display (factorial 5)) (newline)
+        (* n (factorial (- n 1)))))
+    
+(display (factorial 6)) (newline)
 
 (define (factorial n)
     (fact_iter 1 1 n))
@@ -16,21 +14,18 @@
         product
         (fact_iter (* counter product)
                    (+ counter 1)
-                   max_count)))
-(display (factorial 3)) (newline)
-(display (factorial 4)) (newline)
-(display (factorial 5)) (newline)
+                   max_count))) 
+
+(display (factorial 6)) (newline)
 
 (define (factorial n)
     (define (iter product counter)
         (if (> counter n)
             product
             (iter (* counter product) (+ counter 1))))
-        
     (iter 1 1))
-(display (factorial 3)) (newline)
-(display (factorial 4)) (newline)
-(display (factorial 5)) (newline)
+    
+(display (factorial 6)) (newline)
 
 
 ;; tree recursion
