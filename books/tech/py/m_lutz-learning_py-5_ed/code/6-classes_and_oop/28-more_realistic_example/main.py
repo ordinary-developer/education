39f9232d3b12b4003c594a6d3facfb73a797af02
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-15 22:21:43 +0300

    [m_lutz-learning_py-5_ed][ch 28] add code for subchapter 2

--- books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/28-more_realistic_example/main.py
+++ books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/28-more_realistic_example/main.py
@@ -13,5 +13,54 @@ def example_01():
     print(sue.name, sue.pay)
 
 
+# the "step 2: adding behavior methods" subchapter
+def example_02():
+    print('example 01 (the "step 2: adding behavior methods" subchapter) =>')
+
+    name = 'Bob Smith'
+    print(name.split())
+    print(name.split()[-1])
+
+    pay = 100000
+    pay *= 1.10
+    print('%.2f' % pay)
+
+    class Person:
+        def __init__(self, name, job = None, pay = 0):
+            self.name = name
+            self.job = job
+            self.pay = pay
+    
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job = 'dev', pay = 100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.name.split()[-1])
+    sue.pay *= 1.10
+    print('%2.f' % sue.pay)
+
+    class Person:
+        def __init__(self, name, job = None, pay = 0):
+            self.name = name
+            self.job = job
+            self.pay = pay
+
+        def lastName(self):
+            return self.name.split()[-1]
+        
+        def giveRaise(self, percent):
+            self.pay = int(self.pay * (1 + percent))
+
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job = 'dev', pay = 100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
+
+
+
 if __name__ == '__main__':
     example_01()
+    example_02()
