c066c7b945fbd736877892bd72cd6a7f8c0f5512
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-18 22:11:19 +0300

    [m_lutz-learning_py-5_ed][ch 28] add code for subchapter 5

--- books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/28-more_realistic_example/main.py
+++ books/tech/py/m_lutz-learning_py-5_ed/code/6-classes_and_oop/28-more_realistic_example/main.py
@@ -133,8 +133,88 @@ def example_04():
     print('\n')
 
 
+# the "step 5: customizing constructors" subchapter
+def example_05():
+    print('example 05 (the "step 5: customizing constructors" subchapter) =>')
+
+    class Person:
+        def __init__(self, name, job = None, pay = 0):
+            self.name = name
+            self.job = job
+            self.pay = pay
+
+        def lastName(self):
+            return self.name.split()[-1]
+
+        def giveRaise(self, percent):
+            self.pay = int(self.pay * (1 + percent))
+
+        def __repr__(self):
+            return '[Person: %s, %s]' % (self.name, self.pay)
+
+    class Manager(Person):
+        def __init__(self, name, pay):
+            Person.__init__(self, name, 'mgr', pay)
+        
+        def giveRaise(self, percent, bonus = .10):
+            Person.giveRaise(self, percent + bonus)
+
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job = 'dev', pay = 100000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    tom = Manager('Tom Jones', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+
+    class Manager:
+        def __init__(self, name, pay):
+            self.person = Person(name, 'mgr', pay)
+
+        def giveRaise(self, percent, bonus = .10):
+            self.person.giveRaise(percent + bonus)
+
+        def __getattr__(self, attr):
+            return getattr(self.person, attr)
+
+        def __repr__(self):
+            return str(self.person)
+
+    tom = Manager('Tom Jones', 50000)       
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+    print('\n')
+
+    class Department:
+        def __init__(self, *args):
+            self.members = list(args)
+        def addMember(self, person):
+            self.members.append(person)
+        def giveRaises(self, percent):
+            for person in self.members:
+                person.giveRaise(percent)
+        def showAll(self):
+            for person in self.members:
+                print(person)
+
+    bob = Person('Bob Smith') 
+    sue = Person('Sue Jones', job = 'dev', pay = 100000)
+    tom = Manager('Tom Jones', 50000)
+
+    development = Department(bob, sue)
+    development.addMember(tom)
+    development.giveRaises(.10)
+    development.showAll()
+
+
 if __name__ == '__main__':
     example_01()
     example_02()
     example_03()
     example_04()
+    example_05()
