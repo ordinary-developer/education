5fb33807dcff73a26dffb23d055012d1b5afc997
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-23 18:03:21 +0300

    [m_shlee-qt_5_10][ch 36] add an example for QIODevice

---
+++ books/tech/cpp/qt/m_shlee-qt_5_10/ch_36-filesystem/00-QIODevice/main.cpp
@@ -0,0 +1,48 @@
+#include <QIODevice>
+#include <QFile>
+#include <QDebug>
+namespace workspace { // print contents from any QIODevice
+    
+void print(QIODevice * device) {
+    char ch;
+    QString str;
+    
+    device->open(QIODevice::ReadOnly);
+    for (; !device->atEnd(); ) {
+        device->getChar(&ch);
+        str += ch;
+    }
+    device->close();
+    
+    qDebug() << str;
+}    
+
+void run() {
+    QFile file("file.dat");
+    if (not file.open(QIODevice::WriteOnly)) {
+        qDebug() << "err on creating: fatal err";
+        return;
+    }
+    
+    constexpr const int len = 1024;
+    char data[len];
+    if (file.write(data, len) < 0) {
+        qDebug() << "err on writing";
+    }
+    
+    file.close();
+    
+    print(&file);
+    
+    if (not file.remove()) {
+        qDebug() << "err on removing";
+    }
+}
+} // workspace
+
+
+int main(int, char**) {
+    workspace::run();
+    
+    return 0;
+}
