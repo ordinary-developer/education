d1303fb65ced86504fe88c6c03ec3a21dc89c855
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-05-30 20:17:09 +0300

    [m_shlee-qt_5_10][ch 36] slightly update code for QBuffer

--- books/tech/cpp/qt/m_shlee-qt_5_10/ch_36-filesystem/03-QBuffer/main.cpp
+++ books/tech/cpp/qt/m_shlee-qt_5_10/ch_36-filesystem/03-QBuffer/main.cpp
@@ -3,28 +3,31 @@
 #include <QDataStream>
 #include <QFile>
 #include <QDebug>
+namespace example {
 void run() {
     QByteArray data{};
-
+    
     QBuffer buf{&data};
     buf.open(QIODevice::WriteOnly);
     QDataStream out{&buf};
     out << QString{"msg"};
-
-    QFile file{ "file.dat" };
+    
+    QFile file{"file.dat"};
     if (not file.open(QIODevice::WriteOnly)) {
-        qDebug() << "error with file opening for writing";
+        qDebug() << "err with file opening for writing: fatal err";
         return;
     }
     file.write(data);
     file.close();
-
+    
     QFile::remove(file.fileName());
-}
+}    
+} // example
 
 
+#include <QDebug>
 int main(int, char**) {
-    run();
-
+    qDebug() << "example =>"; example::run(); qDebug() << "";
+    
     return 0;
 }
