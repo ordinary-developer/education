3c92a6f1f55641ffc9d95746844ebb3ff0378510
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-07-11 16:37:48 +0300

    [i_cukic-func_progr_in_cpp][ch 09] slightly change the 2-d example
    slightly change (remove visibility specifier indents)
    for the example for sum types (for states) and inheritance

--- books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_09-alg_data_types_and_pattern_matching/02-sum_types_and_inheritance.cpp
+++ books/tech/cpp/std-17/i_cukic-func_progr_in_cpp/ch_09-alg_data_types_and_pattern_matching/02-sum_types_and_inheritance.cpp
@@ -3,64 +3,63 @@
 namespace workspace {
 
 class state_t {
-    protected:
-        state_t(int type)
-            : type{type} {}
-
-    public:
-        virtual ~state_t() {};
-        int type;
+protected:
+    state_t(int type)
+        : type{type} {}
+public:
+    virtual ~state_t() {};
+    int type;
 };
 
 
 class init_t : public state_t {
-    public:
-        enum { id = 0 };
-        init_t() : state_t{id} {}
+public:
+    enum { id = 0 };
+    init_t() : state_t{id} {}
 };
 
 
 class running_t : public state_t {
-    public:
-        enum { id = 1 };
-        running_t() : state_t{id} {}
+public:
+    enum { id = 1 };
+    running_t() : state_t{id} {}
 
-        unsigned count() const { return m_count; }
+    unsigned count() const { return m_count; }
 
-    private:
-        unsigned m_count = 0;
+private:
+    unsigned m_count = 0;
 };
 
 
 class finished_t : public state_t {
-    public:
-        enum { id = 2 };
-        finished_t(unsigned count) : state_t{id}, m_count{count} {}
+public:
+    enum { id = 2 };
+    finished_t(unsigned count) : state_t{id}, m_count{count} {}
 
-        unsigned count() const { return m_count; }
+    unsigned count() const { return m_count; }
 
-    private:
-        unsigned m_count;
+private:
+    unsigned m_count;
 };
 
 
 class program_t {
-    public:
-        program_t() : m_state{ std::make_unique<init_t>() } {}
+public:
+    program_t() : m_state{ std::make_unique<init_t>() } {}
 
-        void start() {
-            assert((init_t::id == m_state->type));
-            m_state = std::make_unique<running_t>();
-        }
+    void start() {
+        assert((init_t::id == m_state->type));
+        m_state = std::make_unique<running_t>();
+    }
 
-        void finish() {
-            assert((running_t::id == m_state->type));
-            auto state = static_cast<running_t*>(m_state.get());
-            m_state = std::make_unique<finished_t>(state->count());
-        }
+    void finish() {
+        assert((running_t::id == m_state->type));
+        auto state = static_cast<running_t*>(m_state.get());
+        m_state = std::make_unique<finished_t>(state->count());
+    }
 
-    private:
-        std::unique_ptr<state_t> m_state;
+private:
+    std::unique_ptr<state_t> m_state;
 };
 
 
