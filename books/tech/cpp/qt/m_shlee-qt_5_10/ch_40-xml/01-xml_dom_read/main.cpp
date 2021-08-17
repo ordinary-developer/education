be7006367c7f9949cb011d038dea6ea97e122e72
ordinary-developer <merely.ordinary.developer@gmail.com>
2021-08-17 21:26:24 +0300

    [m_shlee-qt_5_10][ch 40] add the 1-st example

---
+++ books/tech/cpp/qt/m_shlee-qt_5_10/ch_40-xml/01-xml_dom_read/main.cpp
@@ -0,0 +1,37 @@
+#include <QtXml>
+#include <QDebug>
+
+void traverseNode(QDomNode const& node);
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {  
+    QDomDocument domDoc{};
+    QFile file{"address_book.xml"};
+    if (file.open(QIODevice::ReadOnly)) {
+        if (domDoc.setContent(&file)) {
+            QDomElement domElement = domDoc.documentElement();
+            traverseNode(domElement);
+        }
+        file.close();
+    }
+    
+	return 0; 
+}
+
+void traverseNode(QDomNode const& node) {
+    QDomNode domNode = node.firstChild();
+    while (not domNode.isNull()) {
+        if (domNode.isElement()) {
+            QDomElement domElement = domNode.toElement();
+            if (not domElement.isNull()) {
+                if ("contact" == domElement.tagName()) {
+                    qDebug() << "attr: " << domElement.attribute("number", "");
+                }
+                else {
+                    qDebug() << "TagName: " << domElement.tagName() << "\tText: " << domElement.text();
+                }
+            }
+        }
+        traverseNode(domNode);
+        domNode = domNode.nextSibling();
+    }
+}
