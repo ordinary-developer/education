02a061469ea8fb45b69ef486f7d1948954663338
ordinary-developer <merely.ordinary.developer@gmail.com>
2022-08-16 21:28:55 +0300

    [sicp] update exercise 1.01

--- theory/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_01.scm
+++ theory/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_01.scm
@@ -1,28 +1,59 @@
-(display 10) (newline)
+#lang sicp
 
-(newline)
-(display (+ 5 3 4)) (newline)
-(display (- 9 1)) (newline)
-(display (/ 6 2)) (newline)
-(display (+ (* 2 4) (- 4 6))) (newline)
+; [exercise] ->
+; Below is a sequence of expressions.
+; What is the result printed by the interpreter
+; to each exression?
+; Assume that the sequence is to be evaluated
+; in the order which it is presented
+;
+; 10
+; (+ 5 3 4)
+; (- 9 1)
+; (/ 6 2)
+; (+ (* 2 4) (- 4 6))
+; (define a 3)
+; (define b (+ a 1))
+; (+ a b (* a b))
+; (= a b)
+; (if (and (> b a) (< b (* a b)))
+;     b
+;     a)
+;
+; (cond ((= a 4) 6)
+;       ((= b 4) (+ 6 7 a))
+;       (else 25))
+;
+; (+ 2 (if (> b a) b a))
+;
+; (* (cond ((> a b) a)
+;          ((< a b) b)
+;          (else -1))
+;    (+ a 1))
 
-(newline)
+
+; [solution] ->
+10
+(+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+
 (define a 3)
 (define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+    
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
 
-(display (+ a b (* a b))) (newline)
-(display (= a b)) (newline)
+(+ 2 (if (> b a) b a))
 
-(display
-    (if (and (> b a) (< b (* a b))) b a)) (newline)
-(display 
-    (cond ((= a 4 ) 6)
-          ((= b 4) (+ 6 7 a))
-          (else 25))) (newline)
-(display 
-    (+ 2 (if (> b a) b a))) (newline)
-(display
-    (* (cond ((> a b) a)
-             ((< a b) b)
-             (else -1))
-        (+ a 1))) (newline)
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
