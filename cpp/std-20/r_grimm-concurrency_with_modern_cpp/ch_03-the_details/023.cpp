7f43613c33d7930d3063d55d90e565cf0d5f04a9
ordinary-developer <merely.ordinary.developer@gmail.com>
2022-09-04 21:35:44 +0300

    [r_grimm-concurrency_with_modern_cpp][ch 03] methods of std::thread

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/023.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+
+
+int main() {
+    std::cout << std::boolalpha << std::endl;
+
+    std::cout << "hardware_concurrency() = " << std::thread::hardware_concurrency() << std::endl;
+
+    std::thread t1{ [] { std::cout << "t1 with id = " << std::this_thread::get_id() << std::endl; } };
+    std::thread t2{ [] { std::cout << "t2 with id = " << std::this_thread::get_id() << std::endl; } };
+
+    std::cout << std::endl;
+
+    std::cout << "FROM MAIN: id of t1 " << t1.get_id() << std::endl;
+    std::cout << "FROM MAIN: id of t2 " << t2.get_id() << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "FROM MAIN: id of main = " << std::this_thread::get_id() << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "t1.joinable(): " << t1.joinable() << std::endl;
+    std::cout << "t2.joinable(): " << t2.joinable() << std::endl;
+
+    if (t1.joinable())
+        t1.join();
+
+    if (t2.joinable())
+        t2.join();
+
+    std::cout << std::endl;
+    
+    std::cout << "t1.joinable(): " << t1.joinable() << std::endl;
+    std::cout << "t2.joinable(): " << t2.joinable() << std::endl;
+
+    std::cout << std::endl;
+}
