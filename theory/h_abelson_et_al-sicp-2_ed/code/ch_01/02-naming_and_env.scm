985b622396848ec841e225380ab5f0e03898da86
ordinary-developer <merely.ordinary.developer@gmail.com>
2022-11-02 23:34:58 +0400

    [h_abelson_et_al-sicp-2_ed][ch 01] slightly update code for subchapter 2

---
+++ theory/h_abelson_et_al-sicp-2_ed/code/ch_01/02-naming_and_env.scm
@@ -0,0 +1,14 @@
+#lang sicp
+
+;; Naming and the Environment
+(display "=> Naming and the Environment")(newline)
+
+(define size 2)
+size
+(* 5 size)
+
+(define pi 3.14159)
+(define radius 10)
+(* pi (* radius radius)) 
+(define circumference (* 2 pi radius))
+circumference
