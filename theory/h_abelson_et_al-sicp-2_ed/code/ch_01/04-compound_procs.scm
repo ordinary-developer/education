a76960e528ee881fb1a35bce71480020a2c5b171
ordinary-developer <merely.ordinary.developer@gmail.com>
2022-11-04 23:52:07 +0400

    [h_abelson_et_al-sicp-2_ed][ch 01] rename the folder for subchapter 4

---
+++ theory/h_abelson_et_al-sicp-2_ed/code/ch_01/04-compound_procs.scm
@@ -0,0 +1,17 @@
+#lang sicp
+
+;; Compound Procedures
+(display "=> Compound Procedures")(newline)
+
+(define (square x) (* x x))
+(square 21)
+(square (+ 2 5))
+(square (square 3))
+
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+(sum-of-squares 3 4)
+
+(define (f a)
+    (sum-of-squares (+ a 1) (* a 2)))
+(f 5)    
