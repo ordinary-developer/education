0350e98d5789aff953d9a69d8099480c4244b7ea
ordinary-developer <merely.ordinary.developer@gmail.com>
2022-11-11 00:17:19 +0400

    [h_abelson-sicp-2_ed][ch 01] update ex. 1.4

--- computer_science/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_04.scm
+++ computer_science/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_04.scm
@@ -1,3 +1,5 @@
+#lang sicp
+
 ; [exercise] =>
 ; Observe that our model of evaluation allows for combinations
 ; whose operators are compound expressions.
@@ -12,15 +14,13 @@
 (define (a_plus_abs_b a b)
     ((if (> b 0) + -) a b))
 
-(define require
+(define assert
 (= 3
    (a_plus_abs_b 1 2)
    (a_plus_abs_b 1 (- 2))
    (a_plus_abs_b 3 0))
 )
-
-
-(if require (display "test => [ok]") (display "test => [fail]"))
+assert
 
 ; Notes:
 ; This procedure returns the sum of the "a" parameter and the modulo of the "b" parameter.
