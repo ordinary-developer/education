857994120e1af3ff46a146a9c0bd530a4ac1fd3f
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-03 01:38:23 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] add a new example

--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/023.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/023.cpp
@@ -1,39 +1,38 @@
+#include <chrono>
 #include <iostream>
+#include <mutex>
 #include <thread>
 
+std::mutex gMut;
+
+class Sleeper {
+    public:
+        explicit Sleeper(int & i) : i_{i} {}
+
+        void operator () (int k) {
+            for (unsigned int j = 0; j <= 5; ++j) {
+                std::this_thread::sleep_for(std::chrono::milliseconds{100});
+
+                {
+                    std::lock_guard<std::mutex> _{gMut};
+                    i_ += k;
+                }
+            }
+
+            std::cout << std::this_thread::get_id() << std::endl;
+        }
+
+    private:
+        int & i_;
+};
 
 int main() {
-    std::cout << std::boolalpha << std::endl;
-
-    std::cout << "hardware_concurrency() = " << std::thread::hardware_concurrency() << std::endl;
-
-    std::thread t1{ [] { std::cout << "t1 with id = " << std::this_thread::get_id() << std::endl; } };
-    std::thread t2{ [] { std::cout << "t2 with id = " << std::this_thread::get_id() << std::endl; } };
-
     std::cout << std::endl;
 
-    std::cout << "FROM MAIN: id of t1 " << t1.get_id() << std::endl;
-    std::cout << "FROM MAIN: id of t2 " << t2.get_id() << std::endl;
+    int valSleeper = 1000;
+    std::thread t{Sleeper(valSleeper), 5};
+    if (t.joinable())
+        t.join();
 
-    std::cout << std::endl;
-
-    std::cout << "FROM MAIN: id of main = " << std::this_thread::get_id() << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "t1.joinable(): " << t1.joinable() << std::endl;
-    std::cout << "t2.joinable(): " << t2.joinable() << std::endl;
-
-    if (t1.joinable())
-        t1.join();
-
-    if (t2.joinable())
-        t2.join();
-
-    std::cout << std::endl;
-    
-    std::cout << "t1.joinable(): " << t1.joinable() << std::endl;
-    std::cout << "t2.joinable(): " << t2.joinable() << std::endl;
-
-    std::cout << std::endl;
+    std::cout << "valSleeper = " << valSleeper << std::endl;
 }
