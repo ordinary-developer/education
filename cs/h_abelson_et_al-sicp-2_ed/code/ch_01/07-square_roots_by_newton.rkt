ea7d95121ac48a102f9761ff1a15bd1af5cd8299
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-04 00:37:02 +0400

    [h_abelson-sicp-2_ed][ch 01] add code for subchapter 1.1.7

---
+++ cs/h_abelson_et_al-sicp-2_ed/code/ch_01/07-square_roots_by_newton.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(display "=> Square Roots by Newton's Method")(newline)
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+
+(define (square x) (* x x))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+
+
+(sqrt 9)    
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(square (sqrt 1000))
