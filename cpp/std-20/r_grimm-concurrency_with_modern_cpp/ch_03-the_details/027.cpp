8e8efacd63e608e742ca03591d7ea24edae9aef0
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-08 20:40:23 +0300

    [r_grimm-concurrency_with_modern_cpp][ch 04] add examples for std::mutex

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/027.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <chrono>
+#include <mutex>
+#include <thread>
+
+
+struct CriticalData {
+    std::mutex mut;
+};
+
+void deadLock(CriticalData & a, CriticalData & b) {
+    std::lock(a.mut, b.mut);
+    std::cout << "Thread: " << std::this_thread::get_id() << " get both mutex" << std::endl;
+
+    std::lock_guard<std::mutex> guard1(a.mut, std::adopt_lock);
+    std::cout << "Thread: " << std::this_thread::get_id() << " first mutex" << std::endl;
+
+    std::lock_guard<std::mutex> guard2(b.mut, std::adopt_lock);
+    std::cout << "Thread: " << std::this_thread::get_id() << " second mutex" << std::endl;    
+}
+
+
+int main() {
+    std::cout << std::endl;
+
+    CriticalData c1;
+    CriticalData c2;
+
+    std::thread t1([&] { deadLock(c1, c2); });
+    std::thread t2([&] { deadLock(c2, c1); });
+
+    if (t1.joinable())
+        t1.join();
+
+    if (t2.joinable())
+        t2.join();
+
+    std::cout << std::endl;
+}
