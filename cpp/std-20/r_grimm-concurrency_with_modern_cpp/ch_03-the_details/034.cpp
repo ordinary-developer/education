d590ac8cf0b57056d9aa90958e3b6fd320876767
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-11 02:51:01 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] add code for thread_local

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/034.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+
+std::mutex coutMutex;
+
+thread_local std::string s{"hello from "};
+
+void addThreadLocal(std::string const & s2) {
+    s += s2;
+
+    std::lock_guard<std::mutex> guard{coutMutex};
+    std::cout << s << std::endl;
+    std::cout << "&s: " << &s << std::endl;
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << std::endl;
+
+    std::thread t1{ addThreadLocal, "t1" };
+    std::thread t2{ addThreadLocal, "t2" };
+    std::thread t3{ addThreadLocal, "t3" };
+    std::thread t4{ addThreadLocal, "t4" };
+
+    t1.join(); t2.join(); t3.join(); t4.join();
+}
