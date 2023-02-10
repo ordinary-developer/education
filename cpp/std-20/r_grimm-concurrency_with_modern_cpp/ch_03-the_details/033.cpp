2857dec41bb7dafc094929f45c5d76fe11845497
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-11 02:22:05 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] add the Meyers singleton

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03-the_details/033.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class MySingleton {
+    public:
+        static MySingleton & getInstance() {
+            static MySingleton instance;
+            return instance;
+        }
+
+    private:
+        MySingleton();
+        ~MySingleton();
+        MySingleton(const MySingleton &) = delete;
+        MySingleton & operator = (const MySingleton &) = delete;
+};
+
+MySingleton::MySingleton() = default;
+MySingleton::~MySingleton() = default;
+
+int main() {
+    std::cout << std::endl;
+    MySingleton::getInstance();
+    std::cout << std::endl;
+}
