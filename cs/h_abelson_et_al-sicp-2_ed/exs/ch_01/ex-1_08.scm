a9eb6b6ce13215ea9c83af0a6ae6ae5d5ad5570d
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-02-25 23:33:53 +0400

    [h_abelson-sicp-2_ed][ch 01][ex 1.8] slightly update the code

--- cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_08.scm
+++ cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_08.scm
@@ -1,4 +1,4 @@
-;; [exercise]
+; [exercise] =>
 ; Newton's method for cube roots is based on the fact
 ; that if `y` is an approximation to the cube root of `x`,
 ; then a better approximation is given by the value
@@ -9,48 +9,52 @@
 ; analogous to the square-root procedure
 
 
+; -------------
+; [solution] =>
+#lang sicp
 
-;; [solution]
-(define (cube_root x)    
-    (define (cube_root_iter guess)
-        (if (good_enough? guess)
+; core
+(define (cube-root x)
+    (define (cube-root-iter guess x)
+        (if (good-enough? guess x)
             guess
-            (cube_root_iter (improve guess))))
+            (cube-root-iter (improve guess x) x)))
 
-    (define (good_enough? guess) (< (abs (- (cube guess) x)) 0.001))
-    (define (cube num) (* num num num))
-    (define (improve guess)
+    (define (good-enough? guess x) (< (abs (- (cube guess) x)) 0.001))
+    (define (abs x) (if (< x 0) (- x) x))
+    (define (cube x) (* x x x))
+    (define (improve guess x)
         (/ (+ (/ x (square guess)) (* 2 guess)) 3))
-    (define (square num) (* num num))
+    (define (square x) (* x x))
 
-    (cube_root_iter 1.0 x))
+    (cube-root-iter 1.0 x))
 
-(define (are_equal_f num1 num2) (< (abs (- num1 num2)) 0.001))
-
-
-; test
-(define (OUTPUT_TEST name ret)
-    (if ret
-        (display "[  OK  ]")
-        (display "[FAILED]"))
+; util functions
+(define (output-test name ret)
+    (display (if ret "[  OK  ]" "[FAILED]"))
     (display " ") (display name) (newline))
 
+(define (abs num) (if (< num 0) (- num) num))
+(define (are-equal num1 num2) (< (abs (- num1 num2)) 0.001))
 
-(display "running tests") (newline)
+; test running
+(display "running tests...") (newline)
 
-(define require (and 
-    (are_equal_f 2 (cube_root 8))
-    (are_equal_f 3 (cube_root 27))
-    (are_equal_f 4 (cube_root 64))))
-(OUTPUT_TEST "cube_root with integer numbers" require)
+(define assert-01
+    (and
+        (are-equal 2 (cube-root 8))
+        (are-equal 2 (cube-root 8))))
+(output-test "cube root with integer numbers" assert-01)
 
-(define require (and 
-    (are_equal_f 2.5 (cube_root 15.625))
-    (are_equal_f 3.7 (cube_root 50.653))
-    (are_equal_f 4.9 (cube_root 117.649))))
-(OUTPUT_TEST "cube_root with real numbers" require)    
+(define assert-02
+    (and
+        (are-equal 2.5 (cube-root 15.625))
+        (are-equal 3.7 (cube-root 50.653))
+        (are-equal 4.9 (cube-root 117.649))))
+(output-test "cube root with real numbers" assert-02)
 
-(define require (and 
-    (are_equal_f (- 2) (cube_root (- 8)))
-    (are_equal_f (- 2.5) (cube_root (- 15.625)))
-(OUTPUT_TEST "cube_root with negative numbers" require)    
+(define assert-03
+    (and
+        (are-equal (- 2) (cube-root (- 8)))
+        (are-equal (- 2.5) (cube-root (- 15.625)))))
+(output-test "cube-root with negative numbers" assert-03)
