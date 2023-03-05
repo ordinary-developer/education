65e653405c70c6229a95758e55f4fd03b9f04238
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-03-06 01:12:00 +0400

    [h_abelson-sicp-2_ed][ch 01] slightly update exercise 1.10

--- cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_10.scm
+++ cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_10.scm
@@ -1,4 +1,6 @@
-;; [exercise]
+#lang sicp
+
+; [exercise] =>
 ; The following procedure computes a mathematical function
 ; called Ackermann's function.
 ;; ```
@@ -33,27 +35,31 @@
 
 
 
-;; [solution]
+; -------------
+; [solution] =>
 (define (A x y)
     (cond ((= y 0) 0)
           ((= x 0) (* 2 y))
           ((= y 1) 2)
           (else (A (- x 1) (A x (- y 1))))))
 
-(display "(A 1 10) = ")(display (A 1 10))(newline)
-(display "(A 2 4) = ")(display (A 2 4))(newline)
-(display "(A 3 3) = ")(display (A 3 3))(newline)
-
+(display "'Base function (A x y)' ->") (display "\n")
+(display "(A 1 10) = ") (display (A 1 10)) (display "\n")
+(display "(A 2 4) = ") (display (A 2 4)) (display "\n")
+(display "(A 3 3) = ") (display (A 3 3)) (display "\n")
+(display "\n")
 
 ; let's consider the `(f n)` function:
 ; f(n) = A(0, n) = 2 * n (by definition)
-;
+
 ; verification:
+(display "'(f n) = (A 0 n)' ->") (display "\n")
 (define (f n) (A 0 n))
-(define (f_reduced n) (* 2 n))
-(display (= (f 1) (f_reduced 1))) 
-    (display " ") (display (= (f 5) (f_reduced 5))) 
-    (display " ") (display (= (f 10) (f_reduced 10))) (newline)
+(define (f-reduced n) (* 2 n))
+(display (= (f 1) (f-reduced 1))) (display "\n")
+(display (= (f 5) (f-reduced 5))) (display "\n")
+(display (= (f 10) (f-reduced 10))) (display "\n")
+(display "\n")
 
 
 ; let's consider the `(g n)` function:
@@ -65,22 +71,22 @@
 ; g(n) = 2^(n - 1) * A(1, n - (n - 1)) = 2^(n - 1) * A(1, 1) = 2^(n - 1) * 2 = 2^n
 ; 
 ; so, g(n) = 2^n
-;
-; verification
-(define (g n) (A 1 n))
 
-(define (g_reduced n) 
-    (define (pow x n)
+; verification
+(display "'(g n) = (A 1 n)' ->") (display "\n")
+(define (g n) (A 1 n))
+(define (g-reduced n)
+    (define (pow x exp)
         (define (iter product counter)
-            (if (> counter n)
+            (if (> counter exp)
                 product
                 (iter (* x product) (+ counter 1))))
         (iter 1 1))
     (pow 2 n))
-
-(display (= (g 1) (g_reduced 1)))
-    (display " ") (display (= (g 5) (g_reduced 5)))
-    (display " ") (display (= (g 10) (g_reduced 10))) (newline)
+(display (= (g 1) (g-reduced 1))) (display "\n")
+(display (= (g 5) (g-reduced 5))) (display "\n")
+(display (= (g 10) (g-reduced 10))) (display "\n")
+(display "\n")
 
 
 ; let's consider the `(h n)` function:
@@ -89,34 +95,36 @@
 ; = 2^(A(2, n')) = 2^(A(2, n - 1)) = 2^(h(n - 1))
 ;
 ; so, h(h) = 2^(h(n - 1))
-;
-; verification
-(define (h n) (A 2 n))
 
-(define (h_reduced n)
-    (define (pow x n)
+; verification
+(display "'(h n) = (A 2 n)' ->") (display "\n")
+(define (h n) (A 2 n))
+(define (h-reduced n)
+    (define (pow x exp)
         (define (iter accum counter)
-            (if (> counter n) accum (iter (* x accum) (+ counter 1))))
+            (if (> counter exp) accum (iter (* x accum) (+ counter 1))))
         (iter 1 1))
 
-    ;(if (= n 0) 1 (pow 2 (h_reduced (- n 1)))))    
-    (if (= n 1) 2 (pow 2 (h_reduced (- n 1)))))    
-
-(display (= (h 1) (h_reduced 1)))
-     (display " ") (display (= (h 2) (h_reduced 2)))
-     (display " ") (display (= (h 3) (h_reduced 3)))
-     (display " ") (display (= (h 4) (h_reduced 4))) (newline)
-
+    ; the next two lines can be used interchangeably
+    ;(if (= n 0) 1 (pow 2 (h-reduced (- n 1))))) 
+    (if (= n 1) 2 (pow 2 (h-reduced (- n 1)))))
+(display (= (h 1) (h-reduced 1))) (display "\n")
+(display (= (h 2) (h-reduced 2))) (display "\n")
+(display (= (h 3) (h-reduced 3))) (display "\n")
+(display "\n")
 ; now `(h 5)`, `(h 6)` and so on due to stack overflow of the `pow` function
 
 
 
-;; [auxiliary]
+; -------------
+; [aux notes to the solution]
+(display "'auxiliary implementations for the H function' ->") (display "\n")
+
 ; an iterative process
-(define (h_reduced_i n)
-    (define (pow x n)
+(define (h-reduced-iter-01 n)
+    (define (pow x exp)
         (define (iter accum counter)
-            (if (> counter n) accum (iter (* x accum) (+ counter 1))))
+            (if (> counter exp) accum (iter (* x accum) (+ counter 1))))
         (iter 1 1))
 
     (define (iter accum counter)
@@ -126,35 +134,35 @@
 
     (iter 1 n))
 
-; iterative process with a block structure
-(define (h_reduced_i_aux n)
-    (define (pow x n)
+; an iterative process with a block structure
+(define (h-reduced-iter-02 n)
+    (define (pow x exp)
         (define (iter accum counter)
-            (if (> counter n) accum (iter (* x accum) (+ counter 1))))
+            (if (> counter exp) accum (iter (* x accum) (+ counter 1))))
         (iter 1 1))
 
     (define (iter accum counter)
         (if (= counter n)
             accum
             (iter (pow 2 accum) (+ counter 1))))
-
+            
     (iter 1 0))
 
-; iterative process with a block structure (another variant)
-(define (h_reduced_i_aux2 n)
-    (define (pow x n)
+; an iterative process with a block structure (another variant)
+(define (h-reduced-iter-03 n)
+    (define (pow x exp)
         (define (iter accum counter)
-            (if (> counter n) accum (iter (* x accum) (+ counter 1))))
+            (if (> counter exp) accum (iter (* x accum) (+ counter 1))))
         (iter 1 1))
 
     (define (iter accum counter)
         (if (> counter n)
             accum
             (iter (pow 2 accum) (+ counter 1))))
-
+            
     (iter 1 1))
 
-
-(display (= (h_reduced 2) (h_reduced_i 2) (h_reduced_i_aux 2) (h_reduced_i_aux2 2)))
-    (display " ") (display (= (h_reduced 3) (h_reduced_i 3) (h_reduced_i_aux 3) (h_reduced_i_aux2 3)))
-    (display " ") (display (= (h_reduced 4) (h_reduced_i 4) (h_reduced_i_aux 4) (h_reduced_i_aux2 4))) (newline)
+(display (= (h 2) (h-reduced-iter-01 2) (h-reduced-iter-02 2) (h-reduced-iter-03 2))) (display "\n")
+(display (= (h 3) (h-reduced-iter-01 3) (h-reduced-iter-02 3) (h-reduced-iter-03 3))) (display "\n")
+(display (= (h 4) (h-reduced-iter-01 4) (h-reduced-iter-02 4) (h-reduced-iter-03 4))) (display "\n")
+(display "\n")
