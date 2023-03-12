6d9e72673fd5d26ea864b604cbc126353b042c3a
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-03-13 00:47:44 +0400

    [h_abelson-sicp-2_ed][ch 01] add a solution for ex 1.12

--- cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_12.scm
+++ cs/h_abelson_et_al-sicp-2_ed/exs/ch_01/ex-1_12.scm
@@ -29,8 +29,7 @@
 (define (are-equal num1 num2) (< (abs (- num1 num2)) 0.001))
 
 ; logic
-(define (str-len idx) 
-    (if (= 0 idx) 1 (+ 1 (str-len (- idx 1)))))
+(define (str-len idx) (+ idx 1))    
 
 (define str-len-assert
     (and (are-equal 1 (str-len 0))
@@ -42,21 +41,59 @@
 (output-test "'str-len' func" str-len-assert)
 
 
-(define (low-idx str-idx)
-    (if (= 0 str-idx) 0 (+ 1 (high-idx (- str-idx 1)))))
+(define (low-idx str-idx) 0)
+(define (high-idx str-idx) (- (str-len str-idx) 1))
 
-(define (high-idx str-idx)
-    (if (= 0 str-idx) 0 (- (+ (low-idx str-idx) (str-len str-idx)) 1)))
-    
 (define low-high-idx-assert
     (and (are-equal 0 (low-idx 0))
          (are-equal 0 (high-idx 0))
-         (are-equal 1 (low-idx 1))
-         (are-equal 2 (high-idx 1))
-         (are-equal 3 (low-idx 2))
-         (are-equal 5 (high-idx 2))
-         (are-equal 6 (low-idx 3))
-         (are-equal 9 (high-idx 3))
-         (are-equal 10 (low-idx 4))
-         (are-equal 14 (high-idx 4))))
-(output-test "'low-high-idx' func" low-high-idx-assert)    
+         (are-equal 0 (low-idx 1))
+         (are-equal 1 (high-idx 1))
+         (are-equal 0 (low-idx 2))
+         (are-equal 2 (high-idx 2))
+         (are-equal 0 (low-idx 3))
+         (are-equal 3 (high-idx 3))
+         (are-equal 0 (low-idx 4))
+         (are-equal 4 (high-idx 4))))
+(output-test "'low-high-idx' func" low-high-idx-assert)
+
+
+(define (coef str-idx idx)
+    (cond ((< str-idx 0) (- 1))
+          ((or (< idx 0) (> idx (- (str-len str-idx) 1))) (- 1))
+          ((= str-idx 0) 1)
+          ((= str-idx 1) 1)
+          ((or (= (low-idx str-idx) idx) (= (high-idx str-idx) idx)) 1)
+          (else (+ (coef (- str-idx 1) (- idx 1))
+                   (coef (- str-idx 1) idx)))
+    ) 
+)
+
+(define coef-assert
+    (and (are-equal 1 (coef 0 0))
+         (are-equal 1 (coef 1 0))
+         (are-equal 1 (coef 1 1))
+         (are-equal 1 (coef 2 0))
+         (are-equal 2 (coef 2 1))
+         (are-equal 1 (coef 2 2))
+         (are-equal 1 (coef 3 0))
+         (are-equal 3 (coef 3 1))
+         (are-equal 3 (coef 3 2))
+         (are-equal 1 (coef 3 3))
+         (are-equal 1 (coef 4 0))
+         (are-equal 4 (coef 4 1))
+         (are-equal 6 (coef 4 2))
+         (are-equal 4 (coef 4 3))
+         (are-equal 1 (coef 4 4))
+
+         (are-equal (- 1) (coef (- 1) 0))
+         (are-equal (- 1) (coef (- 10) 0))
+         (are-equal (- 1) (coef 0 1))
+         (are-equal (- 1) (coef 0 (- 1)))
+         (are-equal (- 1) (coef 1 2))
+         (are-equal (- 1) (coef 1 (- 1)))
+         (are-equal (- 1) (coef 2 (str-len 2)))
+         (are-equal (- 1) (coef 3 (str-len 3)))
+         (are-equal (- 1) (coef 4 (str-len 4)))
+    ))
+(output-test "'coef' func" coef-assert)
