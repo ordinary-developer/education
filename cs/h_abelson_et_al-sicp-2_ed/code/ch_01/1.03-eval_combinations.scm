39bff24a52b817d3bfb3bea804804140571c7fd5
ordinary-developer <merely.ordinary.developer@gmail.com>
2023-06-12 20:36:28 +0300

    [h_abelson-sicp-2_ed][ch 01] slightly update the code for subch 1.03

--- cs/h_abelson_et_al-sicp-2_ed/code/ch_01/1.03-eval_combinations.scm
+++ cs/h_abelson_et_al-sicp-2_ed/code/ch_01/1.03-eval_combinations.scm
@@ -1,7 +1,9 @@
 #lang sicp
 
 ;; Evaluating Combinations
-(display "=> Evaluating Combinations") (newline)
+
+(display "[Evaluating Combinations] =>")(newline)
 
 (* (+ 2 (* 4 6))
-   (+ 3 5 7)) 
+   (+ 3 5 7))
+
