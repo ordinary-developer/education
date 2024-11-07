1c293614382a18819a8274537c683a0cb956d992
ordinary-developer <merely.ordinary.developer@gmail.com>
2024-11-07 15:34:46 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] add examples for the std::condition_variable

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/023-predicates_in_cond_var.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+
+std::mutex mutex;
+std::condition_variable condVar;
+
+bool dataReady{false};
+
+void doTheWork() {
+    std::cout << "Processing shared data." << '\n';
+}
+
+void waitingForWork() {
+    std::cout << "Worker: Waiting for work." << '\n';
+
+    std::unique_lock<std::mutex> lock(mutex);
+    while (not []{ return dataReady; }()) {
+        condVar.wait(lock);
+    }
+
+    doTheWork();
+    std::cout << "Work done." << '\n';
+}
+
+void setDataReady() {
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        dataReady = true;
+    }
+    std::cout << "Sender: Data is ready." << '\n';
+    condVar.notify_one();
+}
+
+
+int main() {
+    std::cout << '\n';
+
+    std::thread t1(waitingForWork);
+    std::thread t2(setDataReady);
+    t1.join();
+    t2.join();
+
+    std::cout << '\n';
+}
