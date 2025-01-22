b573797b8f527885abddf1481a3899d509d992ca
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-01-22 20:29:55 +0300

    [r_grimm-concurrency_with_modern_cpp][ch 03] rewrite the example for std::stop_source and std::stop_token

--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/027-sending_signals.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/027-sending_signals.cpp
@@ -7,52 +7,47 @@ using namespace std::literals;
 
 void function1(std::stop_token stopToken, const std::string & str) {
     std::this_thread::sleep_for(1s);
-    if (stopToken.stop_requested()) {
-        std::cout << str << ": Stop requested\n";
-    }
+    if (stopToken.stop_requested())
+        std::cout << "[ .... ] func1, " << str << ": Stop requested" << std::endl;
 }
 
-void function2(std::promise<void> prom,
+void function2(std::promise<void> p,
                std::stop_token stopToken, const std::string & str)
 {
     std::this_thread::sleep_for(1s);
-    std::stop_callback callback(stopToken, [&str] {
-        std::cout << str << ": Stop requested\n";
+    std::stop_callback callback(stopToken, [&str]{
+        std::cout << "[ .... ] func2, " << str << ": Stop requested" << std::endl;
     });
-    prom.set_value();
+    p.set_value();
 }
 
 
 int main() {
-    std::cout << '\n';
+    std::cout << "[  ok  ] starting the app" << std::endl;
 
     std::stop_source stopSource;
 
     std::stop_token stopToken = std::stop_token(stopSource.get_token());
 
     std::thread thr1 = std::thread(function1, stopToken, "std::thread");
-
     std::jthread jthr = std::jthread(function1, stopToken, "std::jthread");
 
-    auto fut1 = std::async([stopToken] {
+    auto aFuture1 = std::async([stopToken] {
         std::this_thread::sleep_for(1s);
-        if (stopToken.stop_requested()) {
-            std::cout << "std::async: Stop requested\n";
-        }
+        if (stopToken.stop_requested())
+            std::cout << "[ .... ] std::async: stop requested" << std::endl;
     });
 
-    std::promise<void> prom;
-    auto fut2 = prom.get_future();
-    std::thread thr2(function2, std::move(prom), stopToken, "std::promise");
+    std::promise<void> aPromise;
+    auto aFuture2 = aPromise.get_future();
+    std::thread thr2(function2, std::move(aPromise), stopToken, "std::promise");
 
     stopSource.request_stop();
-
-    if (stopToken.stop_requested()) {
-        std::cout << "main: Stop requested\n";
-    }
+    if (stopToken.stop_requested())
+        std::cout << "[ .... ] main: Stop requested" << std::endl;
 
     thr1.join();
     thr2.join();
 
-    std::cout << '\n';
+    std::cout << std::endl;
 }
