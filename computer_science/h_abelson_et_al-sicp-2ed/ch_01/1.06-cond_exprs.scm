1b39044a850bad54d030fb3f52ee2163ad08875a
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-03-05 19:54:49 +0300

    [sicp][ch 01] slightly the example for subch 1.06

--- computer_science/h_abelson_et_al-sicp-2ed/ch_01/1.06-cond_exprs.scm
+++ computer_science/h_abelson_et_al-sicp-2ed/ch_01/1.06-cond_exprs.scm
@@ -4,16 +4,20 @@
     (cond ((> x 0) x)
           ((= x 0) 0)
           ((< x 0) (- x))))
+
 (abs1 (- 42))
 (abs1 0)
 (abs1 42)
 
+
 (define (abs2 x)
     (cond ((< x 0) (- x))
           (else x)))
+
 (abs2 (- 42))
 (abs2 0)
 (abs2 42)
+
 
 (define (abs3 x)
     (if (< x 0)
@@ -23,13 +27,16 @@
 (abs3 0)
 (abs3 42)
 
+
 (define x 3)
 (and (> x 5) (< x 10))
+
 
 (define (gt-or-eq x y)
     (or (> x y) (= x y)))
 (gt-or-eq 17 42)
 (gt-or-eq 42 17)
+
 
 (define (gt-or-eq2 x y)
     (not (< x y)))
