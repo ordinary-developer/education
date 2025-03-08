f17dbc478a73eb8eeabd09c40f644a8ff9d8f0c2
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-03-08 23:12:48 +0300

    [h_abelson_et_al-scip-2e][ch 01] slightly modify exercise 1.03

--- computer_science/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_03.scm
+++ computer_science/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_03.scm
@@ -8,23 +8,26 @@
 
 
 ; [solution] =>
-(define (sum-of-squares-of-two-max param1 param2 param3)
-    (define (min2 p1 p2) (if (< p1 p2) p1 p2))    
+(define (sum-of-squares-of-two-larger p1 p2 p3)
+    (define (min2 p1 p2) (if (< p1 p2) p1 p2))
+    (define (min3 p1 p2 p3) (min2 p1 (min2 p2 p3)))
     (define (max2 p1 p2) (if (> p1 p2) p1 p2))
-
-    (define (first-max p1 p2 p3) (max2 (max2 p1 p2) p3))
-    (define (second-max p1 p2 p3)
-        (- (+ p1 p2 p3) (first-max p1 p2 p3) (min2 (min2 p1 p2) p3)))
+    (define (max3 p1 p2 p3) (max2 p1 (max2 p2 p3)))
 
     (define (square p) (* p p))
 
-    (+ (square (first-max param1 param2 param3)) (square (second-max param1 param2 param3))))
+    (+ (square (max3 p1 p2 p3))
+       (square (- (+ p1 p2 p3)
+                  (max3 p1 p2 p3)
+                  (min3 p1 p2 p3)))))
+
 
 (define assert (= 5
-                  (sum-of-squares-of-two-max 0 1 2)
-                  (sum-of-squares-of-two-max 0 2 1)
-                  (sum-of-squares-of-two-max 1 0 2)
-                  (sum-of-squares-of-two-max 1 2 0)
-                  (sum-of-squares-of-two-max 2 0 1)
-                  (sum-of-squares-of-two-max 2 1 0)))
+                  (sum-of-squares-of-two-larger 0 1 2)
+                  (sum-of-squares-of-two-larger 0 2 1)
+                  (sum-of-squares-of-two-larger 1 0 2)
+                  (sum-of-squares-of-two-larger 1 2 0)
+                  (sum-of-squares-of-two-larger 2 0 1)
+                  (sum-of-squares-of-two-larger 2 1 0)))
+
 assert
