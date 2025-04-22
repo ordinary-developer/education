5fd6d68b24d3084b0467804876d41ccdc778d1ba
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-04-22 20:38:18 +0300

    [r_grimm-concurrency_with_modern_cpp][ch 03] change examples for std::stop_callback

---
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/026-invoke_stop_callbacks_after_registration.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <stop_token>
+#include <thread>
+
+using namespace std::literals;
+
+
+using handler_t = std::function<void()>;
+using callback_t = std::stop_callback<handler_t>;
+
+void func(std::stop_token stopToken) {
+	std::list<callback_t> callbacks;
+	for (int i = 0; i <= 9; ++i) {
+		callbacks.emplace_back(stopToken, [i] { std::cout << i; });
+	}
+	std::this_thread::sleep_for(100ms);
+	std::cout << '\n';
+}
+
+
+int main() {
+	std::cout << '\n';
+
+	std::jthread thr1 = std::jthread(func);
+	std::jthread thr2 = std::jthread(func);
+	std::this_thread::sleep_for(50ms);
+	thr1.request_stop();
+	thr2.request_stop();
+
+	std::cout << '\n';
+}
