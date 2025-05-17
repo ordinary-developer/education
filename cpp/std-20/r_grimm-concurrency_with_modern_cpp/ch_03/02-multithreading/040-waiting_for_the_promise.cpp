d82f317c2d9570ef9bb07f4857c5d5f7f6f317f6
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-05-17 15:18:04 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] slightly update examples for communication between std::future and std::promise

--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/040-waiting_for_the_promise.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/040-waiting_for_the_promise.cpp
@@ -3,33 +3,36 @@
 #include <thread>
 #include <chrono>
 
+
 using namespace std::literals::chrono_literals;
 
 void getAnswer(std::promise<int> intPromise) {
-    std::this_thread::sleep_for(3s);
-    intPromise.set_value(42);
+	std::this_thread::sleep_for(3s);
+	intPromise.set_value(42);
 }
 
 
 int main() {
-    std::cout << '\n';
+	std::cout << '\n';
 
-    std::promise<int> answerPromise;
-    auto fut = answerPromise.get_future();
+	std::promise<int> answerPromise;
 
-    std::thread prodThread(getAnswer, std::move(answerPromise));
+	auto fut = answerPromise.get_future();
 
-    std::future_status status{};
-    do {
-        status = fut.wait_for(0.2s);
-        std::cout << "... doing something else" << '\n';
-    } while (status != std::future_status::ready);
+	std::thread prodThread(getAnswer, std::move(answerPromise));
 
-    std::cout << '\n';
+	std::future_status status{};
+	do {
+		status = fut.wait_for(0.2s);
+		std::cout << "... doing something else" << '\n';
+	} while (status != std::future_status::ready);
 
-    std::cout << "The Answer: " << fut.get() << '\n';
+	std::cout << '\n';
 
-    prodThread.join();
+	std::cout << "The answer: " << fut.get() << '\n';
 
-    std::cout << '\n';
+	prodThread.join();
+
+	std::cout << '\n';
 }
+
