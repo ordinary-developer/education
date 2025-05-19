6b4192e7a30aaf6eeeabef10c99bbed4a882622f
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-05-19 22:56:02 +0400

    [r_grimm-concurrency_with_modern_cpp][ch 03] slightly update examples for exceptions for std::future and std::promise

--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/043-promise_future_exception_01.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/043-promise_future_exception_01.cpp
@@ -6,47 +6,44 @@
 
 
 struct Div {
-    void operator()(std::promise<int> && intPromise, int a, int b) {
-        try {
-            if (b == 0) {
-                std::string errMess = std::string("Illegal division by zero: ") +
-                                      std::to_string(a) + "/" + std::to_string(b);
-                throw std::runtime_error(errMess);
-                return;
-            }
-            intPromise.set_value(a / b);
-        }
-        catch (...) {
-            intPromise.set_exception(std::current_exception());
-        }
-    }
+	void operator() (std::promise<int>&& intPromise, int a, int b) {
+		try {
+			if (b == 0) {
+				std::string errMess = std::string("Illegal division by zero: ") +
+					std::to_string(a) + "/" + std::to_string(b);
+				throw std::runtime_error(errMess);
+			}
+			intPromise.set_value(a / b);
+		}
+		catch (...) {
+			intPromise.set_exception(std::current_exception());
+		}
+	}
 };
 
 void executeDivision(int nom, int denom) {
-    std::promise<int> divPromise;
-    std::future<int> divResult = divPromise.get_future();
+	std::promise<int> divPromise;
+	std::future<int> divResult = divPromise.get_future();
 
-    Div div;
-    std::thread divThread(div, std::move(divPromise), nom, denom);
+	Div div;
+	std::thread divThread(div, std::move(divPromise), nom, denom);
 
-    // get the result or the exception
-    try {
-        std::cout << nom << "/" << denom << " = " << divResult.get() << '\n';
-    }
-    catch (std::runtime_error & e) {
-        std::cout << e.what() << '\n';
-    }
+	try {
+		std::cout << nom << "/" << denom << " = " << divResult.get() << '\n';
+	}
+	catch (std::runtime_error& e) {
+		std::cout << e.what() << '\n';
+	}
 
-    divThread.join();
+	divThread.join();
 }
 
 
 int main() {
-    std::cout << '\n';
+	std::cout << '\n';
 
-    executeDivision(20, 0);
-    executeDivision(20, 10);
+	executeDivision(20, 0);
+	executeDivision(20, 10);
 
-    std::cout << '\n';
-    std::cout << "ok" << std::endl;
+	std::cout << '\n';
 }
--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/044-promise_future_exception_02.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/044-promise_future_exception_02.cpp
@@ -6,46 +6,47 @@
 
 
 struct Div {
-    void operator()(std::promise<int> && intPromise, int a, int b) {
-        try {
-            if (b == 0) {
-                std::string errMess = std::string("Illegal division by zero: ") +
-                                      std::to_string(a) + "/" + std::to_string(b);
-                intPromise.set_exception(std::make_exception_ptr(std::runtime_error(errMess)));
-                return;
-            }
-            intPromise.set_value(a / b);
-        }
-        catch (...) {
-            intPromise.set_exception(std::current_exception());
-        }
-    }
+	void operator() (std::promise<int>&& intPromise, int a, int b) {
+		try {
+			if (b == 0) {
+				std::string errMess = std::string("Illegal division by zero: ") +
+					std::to_string(a) + "/" + std::to_string(b);
+				intPromise.set_exception(std::make_exception_ptr(std::runtime_error(errMess)));
+				return;
+			}
+
+			intPromise.set_value(a / b);
+		}
+		catch (...) {
+			intPromise.set_exception(std::current_exception());
+		}
+	}
 };
 
 
 void executeDivision(int nom, int denom) {
-    std::promise<int> divPromise;
-    std::future<int> divResult = divPromise.get_future();
+	std::promise<int> divPromise;
+	std::future<int> divResult = divPromise.get_future();
 
-    Div div;
-    std::thread divThread(div, std::move(divPromise), nom, denom);
+	Div div;
+	std::thread divThread(div, std::move(divPromise), nom, denom);
 
-    try {
-        std::cout << nom << "/" << denom << " = " << divResult.get() << '\n';
-    }
-    catch (std::runtime_error & e) {
-        std::cout << e.what() << '\n';
-    }
+	try {
+		std::cout << nom << "/" << denom << " = " << divResult.get() << '\n';
+	}
+	catch (std::runtime_error& e) {
+		std::cout << e.what() << '\n';
+	}
 
-    divThread.join();
+	divThread.join();
 }
 
 
 int main() {
-    std::cout << '\n';
+	std::cout << '\n';
 
-    executeDivision(20, 0);
-    executeDivision(20, 10);
+	executeDivision(20, 0);
+	executeDivision(20, 10);
 
-    std::cout << '\n';
+	std::cout << '\n';
 }
