ebbbc58ea77c52ad234e90a891f6144bd57be375
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-05-23 22:48:04 +0300

    [r_grimm-concurrency_with_modern_cpp][ch 03] slightly update examples for synch output to std::cout

--- cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/046-sync_access_to_std_cout.cpp
+++ cpp/std-20/r_grimm-concurrency_with_modern_cpp/ch_03/02-multithreading/046-sync_access_to_std_cout.cpp
@@ -1,49 +1,49 @@
 #include <chrono>
 #include <iostream>
 #include <mutex>
-#include <thread>
 
 
 std::mutex coutMutex;
 
 class Worker {
-    public:
-        Worker(std::string n) : name(n) {}
+public:
+	Worker(std::string name) : name_(name) {};
 
-        void operator() () {
-            for (int i = 1; i <= 3; ++i) {
-                // begin work
-                std::this_thread::sleep_for(std::chrono::milliseconds(200));
-                // end work
-                std::lock_guard<std::mutex> coutLock(coutMutex);
-                std::cout << name << ": " << "Work " << i << " done!!!\n";
-            }
-        }
-    private:
-        std::string name;
+	void operator() () {
+		for (int i = 1; i <= 3; ++i) {
+			// begin work
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+			// end work
+			std::lock_guard<std::mutex> coutLock(coutMutex);
+			std::cout << name_ << ": " << "Work " << i << " done !!!\n";
+		}
+	}
+
+private:
+	std::string name_;
 };
 
 
 int main() {
-    std::cout << '\n';
+	std::cout << '\n';
 
-    std::cout << "Boss: Let's start working." << "\n\n";
+	std::cout << "Boss: Let's start working." << "\n\n";
 
-    std::thread herb = std::thread(Worker("Herb"));
-    std::thread andrei = std::thread(Worker("  Andrei"));
-    std::thread scott = std::thread(Worker("    Scott"));
-    std::thread bjarne = std::thread(Worker("      Bjarne"));
-    std::thread bart = std::thread(Worker("        Bart"));
-    std::thread jenne = std::thread(Worker("          Jenne"));
+	std::thread herb = std::thread(Worker("Herb"));
+	std::thread andrei = std::thread(Worker("  Andrei"));
+	std::thread scott = std::thread(Worker("    Scott"));
+	std::thread bjarne = std::thread(Worker("      Bjarne"));
+	std::thread bart = std::thread(Worker("          Bart"));
+	std::thread jenne = std::thread(Worker("            Jenne"));
 
-    herb.join();
-    andrei.join();
-    scott.join();
-    bjarne.join();
-    bart.join();
-    jenne.join();
+	herb.join();
+	andrei.join();
+	scott.join();
+	bjarne.join();
+	bart.join();
+	jenne.join();
 
-    std::cout << "\n" << "Boss: Lef's go home." << '\n';
+	std::cout << "\n" << "Boss: Let's go home." << '\n';
 
-    std::cout << '\n';
+	std::cout << '\n';
 }
