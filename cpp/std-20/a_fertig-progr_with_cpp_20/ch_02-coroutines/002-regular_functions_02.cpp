6b4a274679601ad62cab28bd593812f80570829f
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-05-30 23:27:06 +0300

    [a_fertig-progr_with_cpp_20][ch 02] add examples for regular functions

---
+++ cpp/std-20/a_fertig-progr_with_cpp_20/ch_02-coroutines/002-regular_functions_02.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+void UseCounterValue(int i) {
+	printf("%d\n", i);
+}
+
+int counter() {
+	static int i{ 0 };
+	return i++;
+}
+
+void UseCounter() {
+	for (int i = 0; i < 5; ++i) {
+		UseCounterValue(counter());
+	}
+
+	// other code
+
+	for (int i = 0; i < 5; ++i) {
+		UseCounterValue(counter());
+	}
+}
+
+int main() {
+	UseCounter();
+}
