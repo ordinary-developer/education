035f8c086dbc522d022957dcba23fdb084882b52
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-09-08 22:51:49 +0300

    [m_shlee-qt_5_10][ch 53] slightly update 1-st example

--- cpp/qt/m_shlee-qt_5_10/ch_53-introducing_qt_quick/01-hello_world_01/main.cpp
+++ cpp/qt/m_shlee-qt_5_10/ch_53-introducing_qt_quick/01-hello_world_01/main.cpp
@@ -2,12 +2,12 @@
 #include <QQmlApplicationEngine>
 
 int main(int argc, char** argv) {
-	QGuiApplication app{ argc, argv };
-	
-	QQmlApplicationEngine engine{};
-	engine.load(QUrl{QStringLiteral("qrc:/main.qml")});
-	if (engine.rootObjects().isEmpty())
-		return -1;
-		
-	return app.exec();
+    QGuiApplication app{ argc, argv };
+
+    QQmlApplicationEngine engine{};
+    engine.load(QUrl{QStringLiteral("qrc:/main.qml")});
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
