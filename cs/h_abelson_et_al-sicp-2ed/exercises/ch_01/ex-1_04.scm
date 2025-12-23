71f91b5e7e0f83e22e87df6dfbaac8aad596ec1b
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-12-23 19:46:29 +0300

    [h_abelson_et_al-sicp-2ed][ch 01] slightly rewrite the solution to exercise 1.04

--- cs/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_04.scm
+++ cs/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_04.scm
@@ -3,23 +3,18 @@
 ; [exercise] =>
 ; Observe that our model of evaluation allows for combinations
 ; whose operators are compound expressions.
-; Use this observaration to describe the behavior
-; of the following procedure
+; Use this observation to describe the behavior of the following procedure:
 ; (define (a-plus-abs-b a b)
-;   ((if (> b 0) + -) a b))     
+;   ((if (> b 0) + -) a b))
 
 
 
-; -------------
 ; [solution] =>
 (define (a-plus-abs-b a b)
     ((if (> b 0) + -) a b))
 
-(define assert (= 3
-                  (a-plus-abs-b 1 2)
-                  (a-plus-abs-b 1 (- 2))
-                  (a-plus-abs-b 3 0)))
-assert
+(and (= 3 (a-plus-abs-b 1 (- 2))) (= 5 (a-plus-abs-b 2 (- 3))) (= 7 (a-plus-abs-b 3 (- 4))))
 
-; Note:
-; This procedure returns the sum of the "a" parameter and the absolute value of the "b" parameter.
+
+; note:
+; this procedure returns the sum of the "a" parameter and the absolute value of the "b" parameter
