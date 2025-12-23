0459d13cef6308865ac059f14d73bf220d344c71
ordinary-developer <merely.ordinary.developer@gmail.com>
2025-12-23 19:45:30 +0300

    [h_abelson_et_al-sicp-2ed][ch 01] slightly rewrite the solution to exercise 1.03

--- cs/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_03.scm
+++ cs/h_abelson_et_al-sicp-2ed/exercises/ch_01/ex-1_03.scm
@@ -8,26 +8,31 @@
 
 
 ; [solution] =>
-(define (sum-of-squares-of-two-larger p1 p2 p3)
-    (define (min2 p1 p2) (if (< p1 p2) p1 p2))
-    (define (min3 p1 p2 p3) (min2 p1 (min2 p2 p3)))
-    (define (max2 p1 p2) (if (> p1 p2) p1 p2))
-    (define (max3 p1 p2 p3) (max2 p1 (max2 p2 p3)))
+(define (square x) (* x x))
+(and (= 4 (square 2)) (= 9 (square 3)) (= 16 (square 4)))
 
-    (define (square p) (* p p))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(and (= 5 (sum-of-squares 1 2)) (= 13 (sum-of-squares 2 3)) (= 25 (sum-of-squares 3 4)))
 
-    (+ (square (max3 p1 p2 p3))
-       (square (- (+ p1 p2 p3)
-                  (max3 p1 p2 p3)
-                  (min3 p1 p2 p3)))))
+(define (max2 x y) (if (> x y) x y))
+(and (= 2 (max2 1 2)) (= 3 (max2 2 3)) (= 4 (max2 3 4)))
+
+(define (max3 x y z) (max2 x (max2 y z)))
+(and (= 3 (max3 1 2 3)) (= 4 (max3 2 3 4)) (= 5 (max3 3 4 5)))
+
+(define (min2 x y) (if (< x y) x y))
+(and (= 1 (min2 1 2)) (= 2 (min2 2 3)) (= 3 (min2 3 4)))
+
+(define (min3 x y z) (min2 x (min2 y z)))
+(and (= 1 (min3 1 2 3)) (= 2 (min3 2 3 4)) (= 3 (min3 3 4 5)))
+
+(define (first-largest x y z) (max3 x y z))
+(and (= 3 (first-largest 1 2 3)) (= 4 (first-largest 2 3 4)) (= 5 (first-largest 3 4 5)))
+
+(define (second-largest x y z) (- (+ x y z) (max3 x y z) (min3 x y z)))
+(and (= 2 (second-largest 1 2 3)) (= 3 (second-largest 2 3 4)) (= 4 (second-largest 3 4 5)))
 
 
-(define assert (= 5
-                  (sum-of-squares-of-two-larger 0 1 2)
-                  (sum-of-squares-of-two-larger 0 2 1)
-                  (sum-of-squares-of-two-larger 1 0 2)
-                  (sum-of-squares-of-two-larger 1 2 0)
-                  (sum-of-squares-of-two-larger 2 0 1)
-                  (sum-of-squares-of-two-larger 2 1 0)))
-
-assert
+(define (f x y z)
+    (sum-of-squares (first-largest x y z) (second-largest x y z)))
+(and (= 13 (f 1 2 3)) (= 25 (f 2 3 4)) (= 41 (f 3 4 5)))
